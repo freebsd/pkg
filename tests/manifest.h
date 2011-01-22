1a1b7cd33e4b986a1bd9970c736ffedf2c2cb353
jlaffaye <jlaffaye@FreeBSD.org>
2011-01-22 17:03:39 +0000

    Add test infrastructure.
    devel/libcheck is required to build and run the tests.
    Right now the test on execs fail...

---
+++ tests/manifest.h
@@ -0,0 +1,3 @@
+#include <check.h>
+
+TCase * tcase_manifest(void);
---
+++ tests/test.c
@@ -0,0 +1,20 @@
+#include <check.h>
+
+#include "manifest.h"
+
+int
+main()
+{
+	int nfailed = 0;
+	Suite *s = suite_create("pkgng");
+
+	suite_add_tcase(s, tcase_manifest());
+
+	/* Run the tests ...*/
+	SRunner *sr = srunner_create(s);
+	srunner_set_log(sr, "test.log");
+	srunner_run_all(sr, CK_NORMAL);
+	nfailed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (nfailed == 0 ? 0 : 1);
+}
