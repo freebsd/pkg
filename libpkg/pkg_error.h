2686383409d98488ae7b2c39d531af9ef380e21a
Will Andrews <will@firepipe.net>
2011-05-13 15:37:41 -0600

    Indicate the location of sqlite failures.

--- libpkg/pkg_error.h
+++ libpkg/pkg_error.h
@@ -12,7 +12,7 @@
 	pkg_error_set(EPKG_FATAL, "Bad argument `%s` in %s", name, __FUNCTION__)
 
 #define ERROR_SQLITE(db) \
-	pkg_error_set(EPKG_FATAL, "%s (sqlite)", sqlite3_errmsg(db))
+	pkg_error_set(EPKG_FATAL, "%s (sqlite at %s:%d)", sqlite3_errmsg(db), __FILE__, __LINE__)
 
 pkg_error_t _pkg_error_set(pkg_error_t, const char *, ...);
 
