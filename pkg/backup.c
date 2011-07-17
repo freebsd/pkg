10688aef59106e95c3daba9bdeb27f26538c9962
Marin Atanasov Nikolov <dnaeon@gmail.com>
2011-07-17 23:43:20 +0300

    exec_backup() should return
    	- Return right exit codes
    	- Do not close db if pkgdb_open() fails
    	- Catch errors from pkgdb_dump()

--- pkg/backup.c
+++ pkg/backup.c
@@ -17,7 +17,7 @@ exec_backup(int argc, char **argv)
 
 	if (argc < 1 || argc > 2 || argv[1][0] != '-') {
 		usage_backup();
-		return (-1);
+		return (EX_USAGE);
 	}
 
 	if (argc == 2)
@@ -25,18 +25,21 @@ exec_backup(int argc, char **argv)
 
 	if (pkgdb_open(&db, PKGDB_DEFAULT) != EPKG_OK) {
 		pkg_error_warn("can not open database");
-		pkgdb_close(db);
-		return (EPKG_FATAL);
+		return (EX_IOERR);
 	}
 
 	if (argv[1][1] == 'd') {
 		printf("Dumping database...");
 		fflush(stdout);
-		pkgdb_dump(db, dest);
+		if (pkgdb_dump(db, dest) == EPKG_FATAL)
+			return (EPKG_FATAL);
+
 		printf("Done\n");
 	}
 
 	if (argv[1][1] == 'r') {
 		fprintf(stderr, "not yet implemented\n");
 	}
+
+	return (EPKG_OK);
 }
