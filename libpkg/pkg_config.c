39d8801cc4ff1012bcca04975827eaab74a554ef
jlaffaye <jlaffaye@FreeBSD.org>
2011-07-20 16:11:45 +0100

    Use /var/cache/pkg for PKG_CACHEDIR.

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -18,7 +18,7 @@ struct _config {
 } c[] = {
 	{ "PACKAGESITE", NULL, NULL},
 	{ "PKG_DBDIR", "/var/db/pkg", NULL},
-	{ "PKG_CACHEDIR", "/var/lib/pkg", NULL},
+	{ "PKG_CACHEDIR", "/var/cache/pkg", NULL},
 	{ NULL, NULL, NULL}
 };
 
