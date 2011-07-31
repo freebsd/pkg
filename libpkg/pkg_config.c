1cb311fc10c3d05d546c26ed1b246be471e40bd1
Marin Atanasov Nikolov <dnaeon@gmail.com>
2011-07-31 11:25:26 +0300

    Added a new option PKG_MULTIREPOS
    	- When enabled pkg(1) will work on multi repositories

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -19,6 +19,7 @@ static struct _config {
 	{ "PACKAGESITE", NULL, NULL},
 	{ "PKG_DBDIR", "/var/db/pkg", NULL},
 	{ "PKG_CACHEDIR", "/var/cache/pkg", NULL},
+	{ "PKG_MULTIREPOS", "false", NULL },
 	{ NULL, NULL, NULL}
 };
 
