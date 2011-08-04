f60077377a0385dce3de575634829cde85aa5473
Baptiste Daroussin <bapt@FreeBSD.org>
2011-08-04 17:10:09 +0200

    pkg version -Il '<' now works

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -19,6 +19,7 @@ static struct _config {
 	{ "PACKAGESITE", NULL, NULL},
 	{ "PKG_DBDIR", "/var/db/pkg", NULL},
 	{ "PKG_CACHEDIR", "/var/cache/pkg", NULL},
+	{ "PORTSDIR", "/usr/ports", NULL },
 	{ NULL, NULL, NULL}
 };
 
