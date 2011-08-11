cd086f8416873ef2d5b7baaef7be0ad09bd1ef3e
Baptiste Daroussin <bapt@FreeBSD.org>
2011-08-11 23:31:26 +0200

    Add categories/license/options to search

--- pkg/search.c
+++ pkg/search.c
@@ -27,6 +27,9 @@ exec_search(int argc, char **argv)
 	struct pkgdb *db = NULL;
 	struct pkgdb_it *it = NULL;
 	struct pkg *pkg = NULL;
+	struct pkg_category *cat = NULL;
+	struct pkg_license *lic = NULL;
+	struct pkg_option *opt = NULL;
 
 	while ((ch = getopt(argc, argv, "gxXcd")) != -1) {
 		switch (ch) {
@@ -71,13 +74,30 @@ exec_search(int argc, char **argv)
 		goto cleanup;
 	}
 
-	while (( retcode = pkgdb_it_next(it, &pkg, PKG_LOAD_BASIC)) == EPKG_OK) {
+	while (( retcode = pkgdb_it_next(it, &pkg, PKG_LOAD_BASIC|PKG_LOAD_CATEGORIES|PKG_LOAD_LICENSES|PKG_LOAD_OPTIONS)) == EPKG_OK) {
 		printf("Name: %s\n", pkg_get(pkg, PKG_NAME));
 		printf("Version: %s\n", pkg_get(pkg, PKG_VERSION));
 		printf("Origin: %s\n", pkg_get(pkg, PKG_ORIGIN));
+		printf("Prefix: %s\n", pkg_get(pkg, PKG_PREFIX));
+		printf("Categories:");
+		while (pkg_categories(pkg, &cat) == EPKG_OK)
+			printf(" %s", pkg_category_name(cat));
+		printf("\n");
+		printf("Licenses: ");
+		while (pkg_licenses(pkg, &lic) == EPKG_OK) {
+			printf(" %s", pkg_license_name(lic));
+			if (pkg_licenselogic(pkg) != 1)
+				printf(" %c", pkg_licenselogic(pkg));
+			else
+				printf(" ");
+		}
+		printf("\b \n");
 		printf("Maintainer: %s\n", pkg_get(pkg, PKG_MAINTAINER));
 		printf("WWW: %s\n", pkg_get(pkg, PKG_WWW));
 		printf("Comment: %s\n", pkg_get(pkg, PKG_COMMENT));
+		printf("Options: \n");
+		while (pkg_options(pkg, &opt) == EPKG_OK)
+			printf("\t%s: %s\n", pkg_option_opt(opt), pkg_option_value(opt));
 		humanize_number(size, sizeof(size), pkg_new_flatsize(pkg), "B", HN_AUTOSCALE, 0);
 		printf("Flat size: %s\n", size);
 		humanize_number(size, sizeof(size), pkg_new_pkgsize(pkg), "B", HN_AUTOSCALE, 0);
