b1e6b0b379dd5bdc184807be1a7acc9903e9d374
Baptiste Daroussin <bapt@FreeBSD.org>
2012-03-02 15:55:51 +0100

    Forbid ; in query evalutation format

--- pkg/query.c
+++ pkg/query.c
@@ -340,6 +340,10 @@ format_sql_condition(const char *str, struct sbuf *sqlcond)
 {
 	type_t state = NONE;
 	while (str[0] != '\0') {
+		if (str[0] == ';') {
+			fprintf(stderr, "';' is forbidden in evaluation format");
+			return (EPKG_FATAL);
+		}
 		if (state == NONE) {
 			if (str[0] == '%') {
 				str++;
