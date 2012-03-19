557c7f1b4f3f019775ad2603e43b4e714bbf2a9f
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2012-03-19 13:03:57 +0000

    Drop other copyrights, as this is a new file.

--- pkg/shlib.c
+++ pkg/shlib.c
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) 2011-2012 Baptiste Daroussin <bapt@FreeBSD.org>
- * Copyright (c) 2011-2012 Julien Laffaye <jlaffaye@FreeBSD.org>
- * Copyright (c) 2011-2012 Marin Atanasov Nikolov <dnaeon@gmail.com>
  * Copyright (c) 2012 Matthew Seaman <matthew@FreeBSD.org>
  * All rights reserved.
  * 
