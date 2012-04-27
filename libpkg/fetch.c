ccda7b8e170cdda1a6c5fda5b69c82340bbe4ba8
Baptiste Daroussin <bapt@FreeBSD.org>
2012-04-27 09:21:10 +0200

    Reduce cpu usage while fetching
    Fixes #192

--- libpkg/fetch.c
+++ libpkg/fetch.c
@@ -50,6 +50,8 @@ pkg_fetch_file(const char *url, const char *dest, time_t t)
 	char buf[10240];
 	int retcode = EPKG_OK;
 
+	fetchTimeout = 0;
+
 	if ((fd = open(dest, O_WRONLY|O_CREAT|O_TRUNC|O_EXCL, 0600)) == -1) {
 		pkg_emit_errno("open", dest);
 		return(EPKG_FATAL);
