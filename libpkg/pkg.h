c9dcf3e52f7d40274ee1b8b28428b0ba0c30bec1
Baptiste Daroussin <bapt@FreeBSD.org>
2012-05-04 07:58:58 +0200

    Prepare for beta13

--- libpkg/pkg.h
+++ libpkg/pkg.h
@@ -37,10 +37,10 @@
 #include <sys/sbuf.h>
 #include <openssl/pem.h>
 
-#define PKGVERSION "1.0-beta12"
+#define PKGVERSION "1.0-beta13"
 /* PORTVERSION equivalent for proper pkg-static->ports-mgmt/pkg version comparison
  * in pkgdb_query_newpkgversion() */
-#define PKG_PORTVERSION "1.0.b12"
+#define PKG_PORTVERSION "1.0.b13"
 
 struct pkg;
 struct pkg_dep;
