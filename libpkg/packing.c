2463a526cf41bb14f671bf4d0de26dfb9071d0c7
Baptiste Daroussin <bapt@FreeBSD.org>
2012-06-03 20:52:09 +0200

    Backout the reproducible packages.
    The ports tree and the package building systems are not able to deal correctly
    with it :(

--- libpkg/packing.c
+++ libpkg/packing.c
@@ -133,8 +133,8 @@ packing_append_file_attr(struct packing *pack, const char *filepath, const char 
 	struct stat st;
 	struct archive_entry *entry, *sparse_entry;
 	/* ugly hack for python and emacs */
-	char *p;
-	bool unset_timestamp = true;
+	/*char *p;*/
+	/*bool unset_timestamp = true;*/
 
 	entry = archive_entry_new();
 	archive_entry_copy_sourcepath(entry, filepath);
@@ -171,7 +171,7 @@ packing_append_file_attr(struct packing *pack, const char *filepath, const char 
 		archive_entry_set_perm(entry, perm);
 
 	/* XXX ugly hack for python and emacs */
-	p = strrchr(filepath, '.');
+/*	p = strrchr(filepath, '.');
 
 	if (p != NULL && (strcmp(p, ".pyc") == 0 ||
 	    strcmp(p, ".py") == 0 ||
@@ -186,7 +186,7 @@ packing_append_file_attr(struct packing *pack, const char *filepath, const char 
 		archive_entry_unset_ctime(entry);
 		archive_entry_unset_mtime(entry);
 		archive_entry_unset_birthtime(entry);
-	}
+	}*/
 
 	archive_entry_linkify(pack->resolver, &entry, &sparse_entry);
 
