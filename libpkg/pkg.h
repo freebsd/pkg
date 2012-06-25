05e1746c16be752ed87e1421ffef3529be9d3ee4
Baptiste Daroussin <bapt@FreeBSD.org>
2012-06-25 14:17:26 +0200

    Prepare for beta16

--- libpkg/pkg.h
+++ libpkg/pkg.h
@@ -37,10 +37,10 @@
 #include <sys/sbuf.h>
 #include <openssl/pem.h>
 
-#define PKGVERSION "1.0-beta15"
+#define PKGVERSION "1.0-beta16"
 /* PORTVERSION equivalent for proper pkg-static->ports-mgmt/pkg version comparison
  * in pkgdb_query_newpkgversion() */
-#define PKG_PORTVERSION "1.0.b15_3"
+#define PKG_PORTVERSION "1.0.b16"
 
 struct pkg;
 struct pkg_dep;
