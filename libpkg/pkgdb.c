2920883cd7b31250ca43838a717e1b4b0377adeb
Bryan Drewery <bryan@shatow.net>
2012-07-02 12:24:04 -0500

    Add some copyrights for me

--- libpkg/pkgdb.c
+++ libpkg/pkgdb.c
@@ -5,6 +5,7 @@
  * Copyright (c) 2011 Philippe Pepiot <phil@philpep.org>
  * Copyright (c) 2011-2012 Marin Atanasov Nikolov <dnaeon@gmail.com>
  * Copyright (c) 2012 Matthew Seaman <matthew@FreeBSD.org>
+ * Copyright (c) 2012 Bryan Drewery <bryan@shatow.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
