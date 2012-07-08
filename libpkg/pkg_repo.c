9148d6d25d0f7f486932e7fef56560f8869eef78
Matthew Seaman <matthew@maggot.black-earth.co.uk>
2012-07-08 11:20:04 +0100

    Testing fix.

--- libpkg/pkg_repo.c
+++ libpkg/pkg_repo.c
@@ -605,7 +605,8 @@ pkg_create_repo(char *path, bool force, void (progress)(struct pkg *pkg, void *d
 	while ((ent = fts_read(fts)) != NULL) {
 		const char *name, *version, *origin, *comment, *desc;
 		const char *arch, *maintainer, *www, *prefix;
-		int64_t flatsize, licenselogic;
+		int64_t flatsize;
+		lic_t licenselogic;
 
 		/* skip everything that is not a file */
 		if (ent->fts_info != FTS_F)
