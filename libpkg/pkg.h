4343357e40931487efdf5e8f95d68e6a3e9973e1
Baptiste Daroussin <bapt@FreeBSD.org>
2012-07-30 01:08:25 +0200

    prepare for rc5

--- libpkg/pkg.h
+++ libpkg/pkg.h
@@ -37,10 +37,10 @@
 #include <sys/sbuf.h>
 #include <openssl/pem.h>
 
-#define PKGVERSION "1.0-rc4"
+#define PKGVERSION "1.0-rc5"
 /* PORTVERSION equivalent for proper pkg-static->ports-mgmt/pkg version comparison
  * in pkgdb_query_newpkgversion() */
-#define PKG_PORTVERSION "1.0.r4"
+#define PKG_PORTVERSION "1.0.r5"
 
 struct pkg;
 struct pkg_dep;
