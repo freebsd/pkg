f64b500604fb94a46fccb8f0f570891632293cb0
Baptiste Daroussin <bapt@FreeBSD.org>
2012-08-30 15:50:24 +0200

    Prepare for 1.0

--- libpkg/pkg.h
+++ libpkg/pkg.h
@@ -37,10 +37,10 @@
 #include <sys/sbuf.h>
 #include <openssl/pem.h>
 
-#define PKGVERSION "1.0-rc6"
+#define PKGVERSION "1.0"
 /* PORTVERSION equivalent for proper pkg-static->ports-mgmt/pkg version comparison
  * in pkgdb_query_newpkgversion() */
-#define PKG_PORTVERSION "1.0.r6"
+#define PKG_PORTVERSION "1.0"
 
 struct pkg;
 struct pkg_dep;
