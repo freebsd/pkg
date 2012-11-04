ee98448926ead93674e0af8562eb6ca2ec2448cf
John Marino <draco@marino.st>
2012-11-04 16:04:49 +0100

    Disabled use of jails if /usr/include/jail.h isn't present
    DragonFly does not have a libjail
    
    Conflicts:
    	pkg/main.c

--- pkg/main.c
+++ pkg/main.c
@@ -38,7 +38,9 @@
 #include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
+#ifndef NO_LIBJAIL
 #include <jail.h>
+#endif
 
 #include <pkg.h>
 
@@ -93,10 +95,16 @@ const unsigned int cmd_len = (sizeof(cmd)/sizeof(cmd[0]));
 static void
 usage(void)
 {
-	fprintf(stderr, "usage: pkg [-v] [-d] [-j <jail name or id>|-c <chroot path>] <command> [<args>]\n\n");
+#ifndef NO_LIBJAIL
+ 	fprintf(stderr, "usage: pkg [-v] [-d] [-j <jail name or id>|-c <chroot path>] <command> [<args>]\n\n");
+#else
+	fprintf(stderr, "usage: pkg [-v] [-d] [-c <chroot path>] <command> [<args>]\n\n");
+#endif
 	fprintf(stderr, "Global options supported:\n");
 	fprintf(stderr, "\t%-15s%s\n", "-d", "Increment debug level");
+#ifndef NO_LIBJAIL
 	fprintf(stderr, "\t%-15s%s\n", "-j", "Execute pkg(1) inside a jail(8)");
+#endif
 	fprintf(stderr, "\t%-15s%s\n", "-c", "Execute pkg(1) inside a chroot(8)");
 	fprintf(stderr, "\t%-15s%s\n\n", "-v", "Display pkg(1) version");
 	fprintf(stderr, "Commands supported:\n");
@@ -161,7 +169,9 @@ main(int argc, char **argv)
 	struct commands *command = NULL;
 	unsigned int ambiguous = 0;
 	const char *chroot_path = NULL;
+#ifndef NO_LIBJAIL
 	int jid;
+#endif
 	const char *jail_str = NULL;
 	size_t len;
 	signed char ch;
@@ -178,7 +188,11 @@ main(int argc, char **argv)
 	if (argc < 2)
 		usage();
 
+#ifndef NO_LIBJAIL
 	while ((ch = getopt(argc, argv, "dj:c:vq")) != -1) {
+#else
+	while ((ch = getopt(argc, argv, "d:c:vq")) != -1) {
+#endif
 		switch (ch) {
 		case 'd':
 			debug++;
@@ -186,9 +200,11 @@ main(int argc, char **argv)
 		case 'c':
 			chroot_path = optarg;
 			break;
+#ifndef NO_LIBJAIL
 		case 'j':
 			jail_str = optarg;
 			break;
+#endif
 		case 'v':
 			version++;
 			break;
@@ -222,6 +238,7 @@ main(int argc, char **argv)
 		if (chroot(chroot_path) == -1)
 			errx(EX_SOFTWARE, "chroot failed!");
 
+#ifndef NO_LIBJAIL
 	if (jail_str != NULL) {
 		jid = jail_getid(jail_str);
 		if (jid < 0)
@@ -234,6 +251,7 @@ main(int argc, char **argv)
 	if (jail_str != NULL || chroot_path != NULL)
 		if (chdir("/") == -1)
 			errx(EX_SOFTWARE, "chdir() failed");
+#endif
 
 	if (pkg_init(NULL) != EPKG_OK)
 		errx(EX_SOFTWARE, "Cannot parse configuration file!");
