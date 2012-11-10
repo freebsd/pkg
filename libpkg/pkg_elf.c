ae72cd86e9425958aaed8024c88c6e4f8c199d8c
John Marino <draco@marino.st>
2012-11-10 18:17:42 +0100

    Fix fragile NT_VERSION detection, this also fixed version detection for abi string on DragonFly

--- libpkg/pkg_elf.c
+++ libpkg/pkg_elf.c
@@ -55,6 +55,8 @@
 /* FFR: when we support installing a 32bit package on a 64bit host */
 #define _PATH_ELF32_HINTS       "/var/run/ld-elf32.so.hints"
 
+#define roundup2(x, y)	(((x)+((y)-1))&(~((y)-1))) /* if y is powers of two */
+
 static int
 filter_system_shlibs(const char *name, char *path, size_t pathlen)
 {
@@ -514,7 +516,7 @@ pkg_get_myarch(char *dest, size_t sz)
 		src += note.n_namesz + note.n_descsz;
 	}
 	osname = src;
-	src += note.n_namesz;
+	src += roundup2(note.n_namesz, 4);
 	if (elfhdr.e_ident[EI_DATA] == ELFDATA2MSB)
 		version = be32dec(src);
 	else
