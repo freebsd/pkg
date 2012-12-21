d5e0a33c2001a37065b6a6412891b3b43485ec37
Baptiste Daroussin <bapt@FreeBSD.org>
2012-12-21 18:52:56 +0100

    Add first shell test

---
+++ tests/frontend/version.sh
@@ -0,0 +1,12 @@
+atf_test_case version
+version_head() {
+	atf_set "descr" "testing pkg version"
+}
+
+version_body() {
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1 2 0
+}
+
+atf_init_test_cases() {
+	atf_add_test_case version
+}
