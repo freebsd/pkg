77c0a009b3356b7b27625f59aba74fbc0242e313
Baptiste Daroussin <bapt@FreeBSD.org>
2012-12-21 18:55:31 +0100

    Add more pkg version tests

--- tests/frontend/version.sh
+++ tests/frontend/version.sh
@@ -4,7 +4,10 @@ version_head() {
 }
 
 version_body() {
-	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1 2 0
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1 2
+	atf_check -o inline:">\n" -s exit:0 pkg version -t 2 1
+	atf_check -o inline:"=\n" -s exit:0 pkg version -t 2 2
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 2 1,1
 }
 
 atf_init_test_cases() {
