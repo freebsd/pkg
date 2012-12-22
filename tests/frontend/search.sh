15fd4f746883232a9d63d8f269343e8db90b828f
Baptiste Daroussin <bapt@FreeBSD.org>
2012-12-23 00:56:15 +0100

    Improve the pkg search test case checking stderr is empty and with an extact match for the output

--- tests/frontend/search.sh
+++ tests/frontend/search.sh
@@ -4,7 +4,7 @@ search_head() {
 }
 
 search_body() {
-	atf_check -o match:"New generation package manager" -s exit:0 pkg search -e -Q comment -S name pkg 
+	atf_check -o inline:"pkg                            New generation package manager\n" -e empty -s exit:0 pkg search -e -Q comment -S name pkg
 }
 
 atf_init_test_cases() {
