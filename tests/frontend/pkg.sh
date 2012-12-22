302895ab611aea04847dabf20fd63cc777bdc1bd
Baptiste Daroussin <bapt@FreeBSD.org>
2012-12-22 23:47:10 +0100

    Change test case to match pkg -n change to pkg -N

--- tests/frontend/pkg.sh
+++ tests/frontend/pkg.sh
@@ -4,7 +4,7 @@ pkg_head() {
 }
 
 pkg_body() {
-	atf_check -o empty -e inline:"pkg: package database non-existent\n" -s exit:69 -x PKG_DBDIR=/dev/null pkg -n
+	atf_check -o empty -e inline:"pkg: package database non-existent\n" -s exit:69 -x PKG_DBDIR=/dev/null pkg -N
 	eval `../../newvers.sh`
 	atf_check -o match:"^${PKGVERSION} .*$" -e empty -s exit:0 pkg -v
 	atf_check -o file:pkg-vv.out -e empty -s exit:0 -x 'ABI=freebsd pkg -C "" -vvv | grep -v "^version.*"'
