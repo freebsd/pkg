3211ab48a4a9be513bc8343e3b575fbf7d2e8ce7
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2012-12-22 23:24:14 +0000

    Fix the more egregious errors in the file.

--- tests/frontend/search.sh
+++ tests/frontend/search.sh
@@ -1,11 +1,10 @@
 atf_test_case search
-version_head() {
+search_head() {
 	atf_set "descr" "testing pkg search"
 }
 
-version_body() {
-	atf_check -o inline:"New generation package manager" -s exit:0 \
-	    pkg search -e -Q comment -S name pkg 
+search_body() {
+	atf_check -o match:"New generation package manager" -s exit:0 pkg search -e -Q comment -S name pkg 
 }
 
 atf_init_test_cases() {
