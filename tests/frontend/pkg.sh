e5742f089f9b7052b078912cd793f5eec5fd1d11
Baptiste Daroussin <bapt@FreeBSD.org>
2012-12-22 02:59:46 +0100

    Add a test case for pkg -v

--- tests/frontend/pkg.sh
+++ tests/frontend/pkg.sh
@@ -5,6 +5,8 @@ pkg_head() {
 
 pkg_body() {
 	atf_check -o empty -e inline:"pkg: package database non-existent\n" -s exit:69 -x PKG_DBDIR=/dev/null pkg -n
+	eval `../../newvers.sh`
+	atf_check -o match:"^${PKGVERSION} .*$" -e empty -s exit:0 pkg -v
 }
 
 atf_init_test_cases() {
