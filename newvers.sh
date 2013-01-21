96f6681416b637c63a31fa5820e45ea6e43d373e
Baptiste Daroussin <bapt@FreeBSD.org>
2013-01-21 23:50:23 +0100

    New Release 1.0.5

--- newvers.sh
+++ newvers.sh
@@ -39,9 +39,9 @@
 
 : ${PKG_MAJOR_VERSION:="1"}
 : ${PKG_MINOR_VERSION="0"}
-: ${PKG_PATCH_LEVEL="4"}
+: ${PKG_PATCH_LEVEL="5"}
  
-: ${PORTREVISION:="1"}
+: ${PORTREVISION:=}
 : ${PORTEPOCH:=}
 
 # ------------------------------------------------------------------
