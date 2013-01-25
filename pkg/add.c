017178deb076bfd7c4902a2cd3b2152e13fa6c50
Romain Tartière <romain@blogreen.org>
2013-01-25 18:52:53 +0100

    Set retcode to EPKG_FATAL on failure.

--- pkg/add.c
+++ pkg/add.c
@@ -128,7 +128,7 @@ exec_add(int argc, char **argv)
 	if(failedpkgcount > 0) {
 		sbuf_finish(failedpkgs);
 		printf("\nFailed to install the following %d package(s): %s\n", failedpkgcount, sbuf_data(failedpkgs));
-		retcode = 1;
+		retcode = EPKG_FATAL;
 	}
 	sbuf_delete(failedpkgs);
 
