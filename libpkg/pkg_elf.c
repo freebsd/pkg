bcd8ad8bb81becac7de828151ba7a3dc2d7f0d05
John Marino <draco@marino.st>
2013-02-20 14:11:43 +0100

    Fix ABI on dragonfly
    Define dragonfly as legitimate value for elf notes and also updates the ABI
    version which is different than FreeBSD. FreeBSD only needs whole numbers for
    the version (e.g. 8,9,10) while DragonFly needs evenly numbered point versions
    (e.g. 3.4, 3.6, 3.8).
    
    The development branches are oddly numbered (e.g. 3.3, 3.5) but packages created
    by these branches should be valid for the upcoming release, so 3.3 will round to
    3.4 for the ABI version.

--- libpkg/pkg_elf.c
+++ libpkg/pkg_elf.c
@@ -283,7 +283,8 @@ analyse_elf(struct pkg *pkg, const char *fpath,
 	if (note != NULL) {
 		data = elf_getdata(note, NULL);
 		osname = (const char *) data->d_buf + sizeof(Elf_Note);
-		if (strncasecmp(osname, "freebsd", sizeof("freebsd")) != 0) {
+		if (strncasecmp(osname, "freebsd", sizeof("freebsd")) != 0 &&
+		    strncasecmp(osname, "dragonfly", sizeof("dragonfly")) != 0) {
 			ret = EPKG_END;	/* Foreign (probably linux) ELF object */
 			goto cleanup;
 		}
@@ -558,8 +559,13 @@ pkg_get_myarch(char *dest, size_t sz)
 	wordsize_corres_str = elf_corres_to_string(wordsize_corres,
 	    (int)elfhdr.e_ident[EI_CLASS]);
 
+#if defined(__DragonFly__)
+	snprintf(dest, sz, "%s:%d.%d:%s:%s",
+	    osname, version / 100000, (((version / 100 % 1000)+1)/2)*2,
+#else
 	snprintf(dest, sz, "%s:%d:%s:%s",
 	    osname, version / 100000,
+#endif
 	    elf_corres_to_string(mach_corres, (int) elfhdr.e_machine),
 	    wordsize_corres_str);
 
