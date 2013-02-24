freebsd/pkg

commit 7021b09dfbf88aa085b4f009fdc06f1b9e45cb5f

package locking -- user interface bits
* Add new pkg lock command and pkg-lock.8 man page
* New -k option to pkg info and %k for pkg query
* Update to local.sqlite schema
* Update to struct pkg and struct pkg_dep

libpkg/pkgdb.c
@@ -51,7 +51,7 @@
 #include "private/utils.h"
 
 #include "private/db_upgrades.h"
-#define DBVERSION 12
+#define DBVERSION 13
 
 #define PKGGT	(1U << 1)
 #define PKGLT	(1U << 2)
@@ -103,6 +103,7 @@ static struct column_mapping {
 	{ "pkgsize",	PKG_NEW_PKGSIZE },
 	{ "licenselogic", PKG_LICENSE_LOGIC },
 	{ "automatic",	PKG_AUTOMATIC },
+	{ "locked",	PKG_LOCKED },
 	{ "time",	PKG_TIME },
 	{ "infos",	PKG_INFOS },
 	{ "rowid",	PKG_ROWID },
@@ -479,6 +480,7 @@ pkgdb_init(sqlite3 *sdb)
 		"prefix TEXT NOT NULL,"
 		"flatsize INTEGER NOT NULL,"
 		"automatic INTEGER NOT NULL,"
+		"locked INTEGER NOT NULL DEFAULT 0,"
 		"licenselogic INTEGER NOT NULL,"
 		"infos TEXT, "
 		"time INTEGER, "
@@ -1153,7 +1155,7 @@ pkgdb_query(struct pkgdb *db, const char *pattern, match_t match)
 			"SELECT id, origin, name, version, comment, desc, "
 				"message, arch, maintainer, www, "
 				"prefix, flatsize, licenselogic, automatic, "
-				"time, infos "
+				"locked, time, infos "
 			"FROM packages AS p%s "
 			"ORDER BY p.name;", comp);
 
@@ -1249,7 +1251,6 @@ pkgdb_is_dir_used(struct pkgdb *db, const char *dir, int64_t *res)
 
 	return (EPKG_OK);
 
-
 }
 
 int
@@ -1259,8 +1260,13 @@ pkgdb_load_deps(struct pkgdb *db, struct pkg *pkg)
 	int		 ret = EPKG_OK;
 	char		 sql[BUFSIZ];
 	const char	*reponame = NULL;
-	const char	*basesql = ""
-		"SELECT d.name, d.origin, d.version "
+	const char	*mainsql = ""
+		"SELECT d.name, d.origin, d.version, p.locked "
+		"FROM main.deps AS d, "
+		"main.packages AS p "
+		"WHERE d.package_id = ?1 AND p.origin = d.origin;";
+	const char	*reposql = ""
+		"SELECT d.name, d.origin, d.version, 0 "
 		"FROM %Q.deps AS d "
 		"WHERE d.package_id = ?1;";
 
@@ -1272,11 +1278,12 @@ pkgdb_load_deps(struct pkgdb *db, struct pkg *pkg)
 	if (pkg->type == PKG_REMOTE) {
 		assert(db->type == PKGDB_REMOTE);
 		pkg_get(pkg, PKG_REPONAME, &reponame);
-		sqlite3_snprintf(sizeof(sql), sql, basesql, reponame);
+		sqlite3_snprintf(sizeof(sql), sql, reposql, reponame);
+		ret = sqlite3_prepare_v2(db->sqlite, sql, -1, &stmt, NULL);
 	} else
-		sqlite3_snprintf(sizeof(sql), sql, basesql, "main");
+		ret = sqlite3_prepare_v2(db->sqlite, mainsql, -1, &stmt, NULL);
 
-	if (sqlite3_prepare_v2(db->sqlite, sql, -1, &stmt, NULL) != SQLITE_OK) {
+	if (ret != SQLITE_OK) {
 		ERROR_SQLITE(db->sqlite);
 		return (EPKG_FATAL);
 	}
@@ -1285,7 +1292,9 @@ pkgdb_load_deps(struct pkgdb *db, struct pkg *pkg)
 
 	while ((ret = sqlite3_step(stmt)) == SQLITE_ROW) {
 		pkg_adddep(pkg, sqlite3_column_text(stmt, 0),
-		    sqlite3_column_text(stmt, 1), sqlite3_column_text(stmt, 2));
+			   sqlite3_column_text(stmt, 1),
+			   sqlite3_column_text(stmt, 2),
+			   sqlite3_column_int(stmt, 3));
 	}
 	sqlite3_finalize(stmt);
 
@@ -1307,8 +1316,13 @@ pkgdb_load_rdeps(struct pkgdb *db, struct pkg *pkg)
 	const char	*origin;
 	const char	*reponame = NULL;
 	char		 sql[BUFSIZ];
-	const char	*basesql = ""
-		"SELECT p.name, p.origin, p.version "
+	const char	*mainsql = ""
+		"SELECT p.name, p.origin, p.version, p.locked "
+		"FROM main.packages AS p, main.deps AS d "
+		"WHERE p.id = d.package_id "
+			"AND d.origin = ?1;";
+	const char	*reposql = ""
+		"SELECT p.name, p.origin, p.version, 0 "
 		"FROM %Q.packages AS p, %Q.deps AS d "
 		"WHERE p.id = d.package_id "
 			"AND d.origin = ?1;";
@@ -1321,11 +1335,12 @@ pkgdb_load_rdeps(struct pkgdb *db, struct pkg *pkg)
 	if (pkg->type == PKG_REMOTE) {
 		assert(db->type == PKGDB_REMOTE);
 		pkg_get(pkg, PKG_REPONAME, &reponame);
-		sqlite3_snprintf(sizeof(sql), sql, basesql, reponame, reponame);
+		sqlite3_snprintf(sizeof(sql), sql, reposql, reponame, reponame);
+		ret = sqlite3_prepare_v2(db->sqlite, sql, -1, &stmt, NULL);
 	} else
-		sqlite3_snprintf(sizeof(sql), sql, basesql, "main", "main");
+		ret = sqlite3_prepare_v2(db->sqlite, mainsql, -1, &stmt, NULL);
 
-	if (sqlite3_prepare_v2(db->sqlite, sql, -1, &stmt, NULL) != SQLITE_OK) {
+	if (ret != SQLITE_OK) {
 		ERROR_SQLITE(db->sqlite);
 		return (EPKG_FATAL);
 	}
@@ -1335,7 +1350,9 @@ pkgdb_load_rdeps(struct pkgdb *db, struct pkg *pkg)
 
 	while ((ret = sqlite3_step(stmt)) == SQLITE_ROW) {
 		pkg_addrdep(pkg, sqlite3_column_text(stmt, 0),
-		    sqlite3_column_text(stmt, 1), sqlite3_column_text(stmt, 2));
+			    sqlite3_column_text(stmt, 1),
+			    sqlite3_column_text(stmt, 2),
+			    sqlite3_column_int(stmt, 3));
 	}
 	sqlite3_finalize(stmt);
 
@@ -1879,7 +1896,6 @@ prstmt_finalize(struct pkgdb *db)
 	db->prstmt_initialized = false;
 	return;
 }
-
 
 int
 pkgdb_register_pkg(struct pkgdb *db, struct pkg *pkg, int complete)
@@ -3184,7 +3200,6 @@ pkgdb_rquery(struct pkgdb *db, const char *pattern, match_t match,
 	return (pkgdb_it_new(db, stmt, PKG_REMOTE));
 }
 
-
 static int
 pkgdb_search_build_search_query(struct sbuf *sql, match_t match,
     pkgdb_field field, pkgdb_field sort)
@@ -3269,7 +3284,6 @@ pkgdb_search(struct pkgdb *db, const char *pattern, match_t match,
 	assert(db != NULL);
 	assert(pattern != NULL && pattern[0] != '\0');
 	assert(db->type == PKGDB_REMOTE);
-
 
 	sql = sbuf_new_auto();
 	sbuf_cat(sql, basesql);
@@ -3523,7 +3537,7 @@ pkgdb_vset(struct pkgdb *db, int64_t id, va_list ap)
 {
 	int		 attr;
 	sqlite3_stmt	*stmt;
-	int64_t		 automatic, flatsize;
+	int64_t		 automatic, flatsize, locked;
 	char		*oldorigin;
 	char		*neworigin;
 
@@ -3564,6 +3578,13 @@ pkgdb_vset(struct pkgdb *db, int64_t id, va_list ap)
 			sqlite3_bind_int64(stmt, 1, automatic);
 			sqlite3_bind_int64(stmt, 2, id);
 			break;
+		case PKG_SET_LOCKED:
+			locked = (int64_t)va_arg(ap, int);
+			if (locked != 0 && locked != 1)
+				continue;
+			sqlite3_bind_int64(stmt, 1, locked);
+			sqlite3_bind_int64(stmt, 2, id);
+			break;
 		case PKG_SET_DEPORIGIN:
 			oldorigin = va_arg(ap, char *);
 			neworigin = va_arg(ap, char *);
@@ -3583,7 +3604,7 @@ pkgdb_vset(struct pkgdb *db, int64_t id, va_list ap)
 			sqlite3_finalize(stmt);
 			return (EPKG_FATAL);
 		}
-		
+
 		sqlite3_finalize(stmt);
 	}
 	return (EPKG_OK);
@@ -3917,7 +3938,6 @@ pkgdb_stats(struct pkgdb *db, pkg_stats_t type)
 	while (sqlite3_step(stmt) != SQLITE_DONE) {
 		stats = sqlite3_column_int64(stmt, 0);
 	}
-
 
 	sbuf_finish(sql);
 	sbuf_free(sql);

libpkg/pkg.c
@@ -70,6 +70,7 @@ pkg_new(struct pkg **pkg, pkg_t type)
 	}
 
 	(*pkg)->automatic = false;
+	(*pkg)->locked = false;
 	(*pkg)->type = type;
 	(*pkg)->licenselogic = LICENSE_SINGLE;
 
@@ -95,6 +96,7 @@ pkg_reset(struct pkg *pkg, pkg_t type)
 	pkg->new_pkgsize = 0;
 	pkg->time = 0;
 	pkg->automatic = false;
+	pkg->locked = false;
 	pkg->licenselogic = LICENSE_SINGLE;
 
 	pkg_list_free(pkg, PKG_LICENSES);
@@ -199,6 +201,9 @@ pkg_vget(struct pkg const *const pkg, va_list ap)
 		case PKG_AUTOMATIC:
 			*va_arg(ap, bool *) = pkg->automatic;
 			break;
+		case PKG_LOCKED:
+			*va_arg(ap, bool *) = pkg->locked;
+			break;
 		case PKG_TIME:
 			*va_arg(ap, int64_t *) = pkg->time;
 			break;
@@ -298,6 +303,9 @@ pkg_vset(struct pkg *pkg, va_list ap)
 		switch (attr) {
 		case PKG_AUTOMATIC:
 			pkg->automatic = (int)va_arg(ap, int64_t);
+			break;
+		case PKG_LOCKED:
+			pkg->locked = (int)va_arg(ap, int64_t);
 			break;
 		case PKG_LICENSE_LOGIC:
 			pkg->licenselogic = (lic_t)va_arg(ap, int64_t);
@@ -548,7 +556,7 @@ pkg_addgroup(struct pkg *pkg, const char *name)
 }
 
 int
-pkg_adddep(struct pkg *pkg, const char *name, const char *origin, const char *version)
+pkg_adddep(struct pkg *pkg, const char *name, const char *origin, const char *version, bool locked)
 {
 	struct pkg_dep *d = NULL;
 	const char *n1, *v1;
@@ -571,6 +579,7 @@ pkg_adddep(struct pkg *pkg, const char *name, const char *origin, const char *ve
 	sbuf_set(&d->origin, origin);
 	sbuf_set(&d->name, name);
 	sbuf_set(&d->version, version);
+	d->locked = locked;
 
 	HASH_ADD_KEYPTR(hh, pkg->deps, __DECONST(char *, pkg_dep_get(d, PKG_DEP_ORIGIN)),
 	    strlen(pkg_dep_get(d, PKG_DEP_ORIGIN)), d);
@@ -579,7 +588,7 @@ pkg_adddep(struct pkg *pkg, const char *name, const char *origin, const char *ve
 }
 
 int
-pkg_addrdep(struct pkg *pkg, const char *name, const char *origin, const char *version)
+pkg_addrdep(struct pkg *pkg, const char *name, const char *origin, const char *version, bool locked)
 {
 	struct pkg_dep *d;
 
@@ -593,6 +602,7 @@ pkg_addrdep(struct pkg *pkg, const char *name, const char *origin, const char *v
 	sbuf_set(&d->origin, origin);
 	sbuf_set(&d->name, name);
 	sbuf_set(&d->version, version);
+	d->locked = locked;
 
 	HASH_ADD_KEYPTR(hh, pkg->deps, __DECONST(char *, pkg_dep_get(d, PKG_DEP_ORIGIN)),
 	    strlen(pkg_dep_get(d, PKG_DEP_ORIGIN)), d);

libpkg/pkg.h
@@ -182,6 +182,7 @@ typedef enum {
 	PKG_NEW_PKGSIZE,
 	PKG_LICENSE_LOGIC,
 	PKG_AUTOMATIC,
+	PKG_LOCKED,
 	PKG_ROWID,
 	PKG_TIME,
 } pkg_attr;
@@ -189,6 +190,7 @@ typedef enum {
 typedef enum {
 	PKG_SET_FLATSIZE = 1U,
 	PKG_SET_AUTOMATIC,
+	PKG_SET_LOCKED,
 	PKG_SET_DEPORIGIN,
 	PKG_SET_ORIGIN
 } pkg_set_attr;
@@ -525,9 +527,9 @@ int pkg_set_from_file(struct pkg *pkg, pkg_attr attr, const char *file);
  * @return An error code.
  */
 int pkg_adddep(struct pkg *pkg, const char *name, const char *origin, const
-			   char *version);
+			   char *version, bool locked);
 int pkg_addrdep(struct pkg *pkg, const char *name, const char *origin, const
-			   char *version);
+			   char *version, bool locked);
 
 //int pkg_addfile(struct pkg *, const char *fmt);
 /**
@@ -656,6 +658,7 @@ const char *pkg_dep_get(struct pkg_dep const * const , const pkg_dep_attr);
 const char *pkg_dep_name(struct pkg_dep const * const);
 const char *pkg_dep_origin(struct pkg_dep const * const);
 const char *pkg_dep_version(struct pkg_dep const * const);
+bool pkg_dep_is_locked(struct pkg_dep const * const);
 
 /* pkg_file */
 const char *pkg_file_get(struct pkg_file const * const, const pkg_file_attr);

libpkg/pkg_attributes.c
@@ -101,6 +101,14 @@ pkg_dep_version(struct pkg_dep const * const d)
 	return (sbuf_get(d->version));
 }
 
+bool
+pkg_dep_is_locked(struct pkg_dep const * const d)
+{
+	assert(d != NULL);
+
+	return d->locked;
+}
+
 /*
  * File
  */

libpkg/pkg_elf.c
@@ -135,8 +135,10 @@ test_depends(void *actdata, struct pkg *pkg, const char *name)
 	d = NULL;
 	if (pkgdb_it_next(it, &d, PKG_LOAD_BASIC) == EPKG_OK) {
 		found = false;
-		pkg_get(d, PKG_ORIGIN, &deporigin, PKG_NAME, &depname,
-		    PKG_VERSION, &depversion);
+		pkg_get(d, PKG_ORIGIN,  &deporigin,
+			   PKG_NAME,    &depname,
+			   PKG_VERSION, &depversion,
+			   PKG_LOCKED,  &deplocked);
 
 		dep = NULL;
 		found = false;
@@ -149,7 +151,7 @@ test_depends(void *actdata, struct pkg *pkg, const char *name)
 		if (!found) {
 			pkg_emit_error("adding forgotten depends (%s): %s-%s",
 					pathbuf, depname, depversion);
-			pkg_adddep(pkg, depname, deporigin, depversion);
+			pkg_adddep(pkg, depname, deporigin, depversion, deplocked);
 		}
 		pkg_free(d);
 	}

libpkg/pkg_manifest.c
@@ -589,7 +589,7 @@ pkg_set_deps_from_node(struct pkg *pkg, yaml_node_t *item, yaml_document_t *doc,
 	}
 
 	if (origin != NULL && version != NULL)
-		pkg_adddep(pkg, depname, origin, version);
+		pkg_adddep(pkg, depname, origin, version, false);
 	else
 		pkg_emit_error("Skipping malformed dependency %s", depname);
 

libpkg/private/db_upgrades.h
@@ -232,6 +232,39 @@ static struct db_upgrades {
 	"CREATE INDEX pkg_shlibs_package_id ON pkg_shlibs (package_id);"
 	"CREATE INDEX pkg_directories_directory_id ON pkg_directories (directory_id);"
 	},
+	{13,
+	"ALTER TABLE packages RENAME TO oldpkgs;"
+	"CREATE TABLE packages ("
+		"id INTEGER PRIMARY KEY,"
+		"origin TEXT UNIQUE NOT NULL,"
+		"name TEXT NOT NULL,"
+		"version TEXT NOT NULL,"
+		"comment TEXT NOT NULL,"
+		"desc TEXT NOT NULL,"
+		"mtree_id INTEGER REFERENCES mtree(id) ON DELETE RESTRICT"
+			" ON UPDATE CASCADE,"
+		"message TEXT,"
+		"arch TEXT NOT NULL,"
+		"maintainer TEXT NOT NULL, "
+		"www TEXT,"
+		"prefix TEXT NOT NULL,"
+		"flatsize INTEGER NOT NULL,"
+		"automatic INTEGER NOT NULL,"
+		"locked INTEGER NOT NULL DEFAULT 0,"
+		"licenselogic INTEGER NOT NULL,"
+		"infos TEXT, "
+		"time INTEGER,"
+		"pkg_format_version INTEGER"
+	");"
+	"INSERT INTO packages (id, origin, name, version, comment, desc, "
+		"mtree_id, message, arch, maintainer, www, prefix, flatsize, "
+		"automatic, licenselogic, pkg_format_version) "
+		"SELECT id, origin, name, version, comment, desc, "
+		"mtree_id, message, arch, maintainer, www, prefix, flatsize, "
+		"automatic, licenselogic, pkg_format_version "
+		"FROM oldpkgs;"
+	"DROP TABLE oldpkgs;"
+	},
 
 	/* Mark the end of the array */
 	{ -1, NULL },

libpkg/private/pkg.h
@@ -81,6 +81,7 @@
 struct pkg {
 	struct sbuf	*fields[PKG_NUM_FIELDS];
 	bool		 automatic;
+	bool		 locked;
 	int64_t		 flatsize;
 	int64_t		 new_flatsize;
 	int64_t		 new_pkgsize;
@@ -108,7 +109,8 @@ struct pkg_dep {
 	struct sbuf	*origin;
 	struct sbuf	*name;
 	struct sbuf	*version;
-	UT_hash_handle	hh;
+	UT_hash_handle	 hh;
+	bool		 locked;
 };
 
 struct pkg_license {

commit 4ac641004f3c28eb6d249cfcdea914d3c6cb4961

Fix `pkg check -d` and `pkg query %d` not returning missing deps
This needs to be a LEFT JOIN instead of INNER JOIN since the
package entry may be missing. It's still a valid dependency
though and should show up under PKG_LOAD_DEPS

Broken by 7021b09df

libpkg/pkgdb.c
@@ -1486,9 +1486,9 @@ pkgdb_load_deps(struct pkgdb *db, struct pkg *pkg)
 	const char	*reponame = NULL;
 	const char	*mainsql = ""
 		"SELECT d.name, d.origin, d.version, p.locked "
-		"FROM main.deps AS d, "
-		"main.packages AS p "
-		"WHERE d.package_id = ?1 AND p.origin = d.origin;";
+		"FROM main.deps AS d "
+		"LEFT JOIN main.packages AS p ON p.origin = d.origin "
+		"WHERE d.package_id = ?1;";
 	const char	*reposql = ""
 		"SELECT d.name, d.origin, d.version, 0 "
 		"FROM %Q.deps AS d "