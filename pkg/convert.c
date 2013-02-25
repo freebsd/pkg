b1c06ce25b473eac00522838251c9a27e0cc3eee
Bryan Drewery <bryan@shatow.net>
2013-02-25 06:43:57 -0600

    Use consistent 'usage:' syntax

--- pkg/convert.c
+++ pkg/convert.c
@@ -39,7 +39,7 @@
 void
 usage_convert(void)
 {
-	fprintf(stderr, "usage pkg convert [-r]\n\n");
+	fprintf(stderr, "usage: pkg convert [-r]\n\n");
 	fprintf(stderr, "For more information see 'pkg help convert'.\n");
 }
 
