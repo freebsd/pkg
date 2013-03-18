b3ece597e5ac7fb19b73841af4a2cce17eb271f3
Bryan Drewery <bryan@shatow.net>
2013-03-17 22:16:30 -0500

    Update gio modules after deinstalling files
    This was causing the module cache file to remain as a leftover even
    after all modules were removed. This could be seen with testing
    devel/gio-fam-backend

--- libpkg/pkg_ports.c
+++ libpkg/pkg_ports.c
@@ -363,6 +363,7 @@ meta_exec(struct plist *p, char *line, bool unexec)
 		    strstr(cmd, "mkfontscale") || strstr(cmd, "mkfontdir") ||
 		    strstr(cmd, "fc-cache") || strstr(cmd, "fonts.dir") ||
 		    strstr(cmd, "fonts.scale") ||
+		    strstr(cmd, "gio-querymodules") ||
 		    strstr(cmd, "gtk-update-icon-cache") ||
 		    strstr(cmd, "update-desktop-database") ||
 		    strstr(cmd, "update-mime-database")) {
