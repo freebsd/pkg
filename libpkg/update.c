2fb79a7c25a5e648bd25e8a6292fc593b74a2da0
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-08 00:01:15 +0200

    Only print once a bad abi during pkg update
    Reported by:	tuxillo (#dragonflybsd on efnet)
    
    Conflicts:
    	libpkg/update.c

--- libpkg/update.c
+++ libpkg/update.c
@@ -243,7 +243,7 @@ pkg_update(const char *name, const char *packagesite, bool force)
 	pkg_config_string(PKG_CONFIG_ABI, &myarch);
 
 	req = sqlite3_mprintf("select group_concat(arch, ', ') from "
-	    "(select arch from packages "
+	    "(select distinct arch from packages "
 	    "where arch not GLOB '%q')", myarch);
 	if (get_sql_string(sqlite, req, &bad_abis) != EPKG_OK) {
 		sqlite3_free(req);
