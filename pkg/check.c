b74400af24c011a5616336663ff53a5fb935df46
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-09 19:02:59 +0200

    Remove last shlib test in pkg check

--- pkg/check.c
+++ pkg/check.c
@@ -255,7 +255,6 @@ exec_check(int argc, char **argv)
 	bool checksums = false;
 	bool recompute = false;
 	bool reanalyse_shlibs = false;
-	bool shlibs;
 	bool noinstall = false;
 	int nbpkgs = 0;
 	int i;
@@ -271,10 +270,6 @@ exec_check(int argc, char **argv)
 			match = MATCH_ALL;
 			break;
 		case 'B':
-			pkg_config_bool(PKG_CONFIG_SHLIBS, &shlibs);
-			if (!shlibs)
-				errx(EX_USAGE, "reanalyzing shlibs requires SHLIBS"
-					       " in pkg.conf.");
 			reanalyse_shlibs = true;
 			flags |= PKG_LOAD_FILES;
 			break;
