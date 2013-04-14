1cb781a965eb8a81e9533fa26fa9ab0f0fad6c54
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2013-04-14 21:56:52 +0100

    Use the freshly compled pkg(8) and libpkg(3) in the source tree for testing.

--- tests/frontend/pkg.sh
+++ tests/frontend/pkg.sh
@@ -77,6 +77,8 @@ pkg_config_defaults_body()
 
 
 atf_init_test_cases() {
+        eval `cat $(atf_get_srcdir)/test_environment`
+
 	atf_add_test_case pkg_no_database
 	atf_add_test_case pkg_version
 	atf_add_test_case pkg_config_defaults
