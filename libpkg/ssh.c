85b5acf17651d90ea62324a223e5655404ebdc28
Baptiste Daroussin <bapt@FreeBSD.org>
2013-04-20 00:06:52 +0200

    Fix copyright, I only hacked on this on 2013

--- libpkg/ssh.c
+++ libpkg/ssh.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2011-2013 Baptiste Daroussin <bapt@FreeBSD.org>
+ * Copyright (c) 2013 Baptiste Daroussin <bapt@FreeBSD.org>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
