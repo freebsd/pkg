41278e31cc11e00bd96261332d228f519f3bd681
Baptiste Daroussin <bapt@FreeBSD.org>
2013-06-07 19:02:26 +0200

    Fix pkg convert

--- libpkg/pkg_old.c
+++ libpkg/pkg_old.c
@@ -94,9 +94,9 @@ pkg_old_load_from_path(struct pkg *pkg, const char *path)
 			pkg_addscript_file(pkg, fpath);
 	}
 
-	pkg_set(pkg, PKG_ARCH, pkg_get_myarch(myarch, BUFSIZ));
-	pkg_set(pkg, PKG_MAINTAINER, "unknown");
 	pkg_get_myarch(myarch, BUFSIZ);
+	pkg_set(pkg, PKG_ARCH, myarch);
+	pkg_set(pkg, PKG_MAINTAINER, "unknown");
 	pkg_get(pkg, PKG_DESC, &desc);
 	regcomp(&preg, "^WWW:[[:space:]]*(.*)$", REG_EXTENDED|REG_ICASE|REG_NEWLINE);
 	if (regexec(&preg, desc, 2, pmatch, 0) == 0) {
