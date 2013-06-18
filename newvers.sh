cbd16e4610b99d6602363b7bb01807a8b4b26ba4
Baptiste Daroussin <bapt@FreeBSD.org>
2013-06-18 21:48:03 +0200

    Bump port revision to match the ports tree

--- newvers.sh
+++ newvers.sh
@@ -41,7 +41,7 @@
 : ${PKG_MINOR_VERSION="0"}
 : ${PKG_PATCH_LEVEL="13"}
  
-: ${PORTREVISION:=""}
+: ${PORTREVISION:="1"}
 : ${PORTEPOCH:=}
 
 # ------------------------------------------------------------------
