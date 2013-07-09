9b799f6b840f627c024a8a3c3e08d5604a4998b4
Mark Felder <feld@feld.me>
2013-07-09 07:05:53 -0500

    speling ;)

--- pkg/upgrade.c
+++ pkg/upgrade.c
@@ -148,7 +148,7 @@ exec_upgrade(int argc, char **argv)
 
 	if (!quiet || dry_run) {
 		print_jobs_summary(jobs,
-		    "Uprgades have been requested for the following %d "
+		    "Upgrades have been requested for the following %d "
 		    "packages:\n\n", nbactions);
 
 		if (!yes && !dry_run)
