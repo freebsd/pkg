9968ceb1f596dcb35abfd59818844dea8233e583
Baptiste Daroussin <bapt@FreeBSD.org>
2013-09-10 17:54:33 +0200

    Add a function to compute a sha256 hash out of a buffer

--- libpkg/utils.c
+++ libpkg/utils.c
@@ -350,6 +350,22 @@ sha256_file(const char *path, char out[SHA256_DIGEST_LENGTH * 2 + 1])
 }
 
 int
+sha256_buf(char *buf, size_t len, char out[SHA256_DIGEST_LENGTH * 2 + 1])
+{
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+
+	out[0] = '\0';
+
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, buf, len);
+	SHA256_Final(hash, &sha256);
+	sha256_hash(hash, out);
+
+	return (EPKG_OK);
+}
+
+int
 sha256_fd(int fd, char out[SHA256_DIGEST_LENGTH * 2 + 1])
 {
 	int my_fd = -1;
