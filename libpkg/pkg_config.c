0f50cc29fd790532adf8f26c46c2e63c14363a4b
Baptiste Daroussin <bapt@FreeBSD.org>
2013-11-06 15:09:31 +0100

    Revert "Do not deprecate yet packagesite as we are not ready for that yet"
    This reverts commit 0339c8da40c1f957b18d6b2f4a55abbcf0404b28.

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -900,6 +900,8 @@ load_repositories(const char *repodir)
 	pkg_config_string(PKG_CONFIG_MIRRORS, &mirror_type);
 
 	if (url != NULL) {
+		pkg_emit_error("PACKAGESITE in pkg.conf is deprecated. "
+		    "Please create a repository configuration file");
 		r = pkg_repo_new("packagesite", url);
 		if (pub != NULL) {
 			r->pubkey = strdup(pub);
