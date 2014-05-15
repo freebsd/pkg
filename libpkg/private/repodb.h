14e2c27a7260096eb47c964d4496996d69b3420d
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-05-15 10:12:02 -0400

    Fix repo upgrade sql.
    Reported by:	az

--- libpkg/private/repodb.h
+++ libpkg/private/repodb.h
@@ -336,8 +336,8 @@ static const struct repo_changes repo_upgrades[] = {
 
 	 "CREATE VIRTUAL TABLE %Q.pkg_search USING fts4(id, name, origin);"
 	 "INSERT INTO %Q.pkg_search SELECT id, name || '-' || version, origin FROM %Q.packages;"
-	 "CREATE INDEX %Q.packages_origin ON %Q.packages(origin COLLATE NOCASE);"
-	 "CREATE INDEX %Q.packages_name ON %Q.packages(name COLLATE NOCASE);"
+	 "CREATE INDEX %Q.packages_origin ON packages(origin COLLATE NOCASE);"
+	 "CREATE INDEX %Q.packages_name ON packages(name COLLATE NOCASE);"
 	},
 	/* Mark the end of the array */
 	{ -1, -1, NULL, NULL, }
