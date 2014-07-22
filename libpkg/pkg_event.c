6208345876e1c31f09351226a83f9e0a638a4fca
Baptiste Daroussin <bapt@FreeBSD.org>
2014-07-22 15:16:31 +0200

    Make formatting consistent

--- libpkg/pkg_event.c
+++ libpkg/pkg_event.c
@@ -336,7 +336,7 @@ pipeevent(struct pkg_event *ev)
 			ev->e_incremental_update.processed);
 		break;
 	case PKG_EVENT_QUERY_YESNO:
-		sbuf_printf(msg, "{\"type\": \"QUERY_YESNO\", "
+		sbuf_printf(msg, "{ \"type\": \"QUERY_YESNO\", "
 		    "\"data\": {"
 			"\"msg\": \"%s\","
 			"\"default\": \"%d\""
@@ -344,7 +344,7 @@ pipeevent(struct pkg_event *ev)
 			ev->e_query_yesno.deft);
 		break;
 	case PKG_EVENT_QUERY_SELECT:
-		sbuf_printf(msg, "{\"type\": \"QUERY_SELECT\", "
+		sbuf_printf(msg, "{ \"type\": \"QUERY_SELECT\", "
 		    "\"data\": {"
 			"\"msg\": \"%s\","
 			"\"ncnt\": \"%d\","
@@ -366,7 +366,7 @@ pipeevent(struct pkg_event *ev)
 		  "\"data\": {}}");
 		break;
 	case PKG_EVENT_PROGRESS_TICK:
-		sbuf_printf(msg, "{\"type\": \"PROGRESS_TICK\", "
+		sbuf_printf(msg, "{ \"type\": \"PROGRESS_TICK\", "
 		  "\"data\": { \"current\": %ld, \"total\" : %ld}}",
 		  ev->e_progress_tick.current, ev->e_progress_tick.total);
 		break;
