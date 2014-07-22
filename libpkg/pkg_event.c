0212548f19e91a8444bad02d7460ea2a44d511b3
Baptiste Daroussin <bapt@FreeBSD.org>
2014-07-22 15:21:56 +0200

    Let the message type name be consistent with others

--- libpkg/pkg_event.c
+++ libpkg/pkg_event.c
@@ -362,11 +362,11 @@ pipeevent(struct pkg_event *ev)
 			ev->e_query_select.items[i]);
 		break;
 	case PKG_EVENT_PROGRESS_START:
-		sbuf_printf(msg, "{\"type\": \"PROGRESS_START\", "
+		sbuf_printf(msg, "{\"type\": \"INFO_PROGRESS_START\", "
 		  "\"data\": {}}");
 		break;
 	case PKG_EVENT_PROGRESS_TICK:
-		sbuf_printf(msg, "{ \"type\": \"PROGRESS_TICK\", "
+		sbuf_printf(msg, "{ \"type\": \"INFO_PROGRESS_TICK\", "
 		  "\"data\": { \"current\": %ld, \"total\" : %ld}}",
 		  ev->e_progress_tick.current, ev->e_progress_tick.total);
 		break;
