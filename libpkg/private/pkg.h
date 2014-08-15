da2a34d4221c37bd1ca686850a7d39f384765668
Bryan Drewery <bryan@shatow.net>
2014-08-15 15:32:15 -0500

    Get rid of pkg_register_shlibs() and use pkg_analyse_files() in pkg create.
    This avoids bugs such as ca3dd25a6.
    The code was nearly identical, update it to also clear the list of
    provided/required shlibs when called as well.
    
    This also fixes pkg create not calling analyse_fpath().
    
    This also enables DEVELOPER_MODE strict shlib analysis in pkg create.

--- libpkg/private/pkg.h
+++ libpkg/private/pkg.h
@@ -504,8 +504,6 @@ int pkgdb_register_finale(struct pkgdb *db, int retcode);
 int pkgdb_set_pkg_digest(struct pkgdb *db, struct pkg *pkg);
 int pkgdb_is_dir_used(struct pkgdb *db, const char *dir, int64_t *res);
 
-int pkg_register_shlibs(struct pkg *pkg, const char *root);
-
 int pkg_emit_manifest_sbuf(struct pkg*, struct sbuf *, short, char **);
 int pkg_emit_filelist(struct pkg *, FILE *);
 
