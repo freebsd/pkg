4b2c1deb24342ec3685598d08529f0c710110e40
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-01-31 11:39:31 +0000

    Workaround for old libarchive.

--- libpkg/private/pkg.h
+++ libpkg/private/pkg.h
@@ -76,6 +76,17 @@
 #define archive_write_add_filter_none(a) archive_write_set_compression_none(a)
 #define archive_read_support_filter_all(a) archive_read_support_compression_all(a)
 #define archive_read_support_filter_none(a) archive_read_support_compression_none(a)
+#define archive_read_free archive_read_finish
+#define archive_write_free archive_write_finish
+
+#ifndef UF_NOUNLINK
+#define UF_NOUNLINK 0
+#endif
+
+#ifndef SF_NOUNLINK
+#define SF_NOUNLINK 0
+#endif
+
 #endif
 
 #define EXTRACT_ARCHIVE_FLAGS  (ARCHIVE_EXTRACT_OWNER |ARCHIVE_EXTRACT_PERM | \
