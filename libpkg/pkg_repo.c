1401ebc37b5b5176ec470232d91f603d63f60896
Baptiste Daroussin <bapt@FreeBSD.org>
2015-02-03 17:54:07 +0100

    Fix previous commit

--- libpkg/pkg_repo.c
+++ libpkg/pkg_repo.c
@@ -643,8 +643,9 @@ pkg_repo_archive_extract_check_archive(int fd, const char *file,
     const char *dest, struct pkg_repo *repo, int dest_fd)
 {
 	struct sig_cert *sc = NULL, *s, *stmp;
+	int ret, rc;
 
-	int ret = rc = EPKG_OK;
+	ret = rc = EPKG_OK;
 
 	if (pkg_repo_archive_extract_archive(fd, file, dest, repo, dest_fd, &sc)
 			!= EPKG_OK)
