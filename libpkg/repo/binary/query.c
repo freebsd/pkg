6c0878a603b2a3900d764bc558a236f1bd7c1409
Baptiste Daroussin <bapt@FreeBSD.org>
2015-02-04 23:21:31 +0100

    Improve debug message

--- libpkg/repo/binary/query.c
+++ libpkg/repo/binary/query.c
@@ -128,7 +128,8 @@ pkg_repo_binary_query(struct pkg_repo *repo, const char *pattern, match_t match)
 	sbuf_cat(sql, " ORDER BY name;");
 	sbuf_finish(sql);
 
-	pkg_debug(4, "Pkgdb: running '%s' query for %s", sbuf_get(sql), pattern);
+	pkg_debug(4, "Pkgdb: running '%s' query for %s", sbuf_get(sql),
+	     pattern == NULL ? "all": pattern);
 	ret = sqlite3_prepare_v2(sqlite, sbuf_get(sql), sbuf_size(sql), &stmt, NULL);
 	if (ret != SQLITE_OK) {
 		ERROR_SQLITE(sqlite, sbuf_get(sql));
