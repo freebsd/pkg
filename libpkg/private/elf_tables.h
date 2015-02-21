6e930005a7fd74efe04f66141cb0741ab3a302a5
Pierre Pronchery <khorben@defora.org>
2015-02-21 12:50:44 +0100

    Not every platform supports aarch64 yet

--- libpkg/private/elf_tables.h
+++ libpkg/private/elf_tables.h
@@ -33,7 +33,9 @@ struct _elf_corres {
 static const struct _elf_corres mach_corres[] = {
 	{ EM_386, "x86" },
 	{ EM_X86_64, "x86" },
+#ifdef EM_AARCH64
 	{ EM_AARCH64, "aarch64" },
+#endif
 	{ EM_ARM, "arm" },
 	{ EM_MIPS, "mips" },
 	{ EM_PPC, "powerpc" },
