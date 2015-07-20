5fb24348b8f0343a95c5523068def994ba8dfac9
Baptiste Daroussin <bapt@FreeBSD.org>
2015-07-20 23:07:30 +0200

    Speed up pkg update by turning locking_mode into exclusive

--- libpkg/repo/binary/update.c
+++ libpkg/repo/binary/update.c
@@ -510,6 +510,7 @@ pkg_repo_binary_update_proceed(const char *name, struct pkg_repo *repo,
 	sql_exec(sqlite, "PRAGMA mmap_size = 209715200;");
 	sql_exec(sqlite, "PRAGMA page_size = %d;", getpagesize());
 	sql_exec(sqlite, "PRAGMA foreign_keys = OFF;");
+	sql_exec(sqlite, "PRAGMA locking_mode = EXCLUSIVE;");
 
 	rc = pkgdb_transaction_begin_sqlite(sqlite, "REPO");
 	if (rc != EPKG_OK)
