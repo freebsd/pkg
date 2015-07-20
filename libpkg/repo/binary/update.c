e9b90205a55ee7ba5a1e12f47af442ce89240621
Baptiste Daroussin <bapt@FreeBSD.org>
2015-07-20 23:01:02 +0200

    Setting specific cache_size is not needed anymore with mmap

--- libpkg/repo/binary/update.c
+++ libpkg/repo/binary/update.c
@@ -509,7 +509,6 @@ pkg_repo_binary_update_proceed(const char *name, struct pkg_repo *repo,
 	/* 200MB should be enough */
 	sql_exec(sqlite, "PRAGMA mmap_size = 209715200;");
 	sql_exec(sqlite, "PRAGMA page_size = %d;", getpagesize());
-	sql_exec(sqlite, "PRAGMA cache_size = 10000;");
 	sql_exec(sqlite, "PRAGMA foreign_keys = OFF;");
 
 	rc = pkgdb_transaction_begin_sqlite(sqlite, "REPO");
