b4623523b99e928a9633c155fc3b99c4abf40a67
Sam Clegg <sbc@chromium.org>
2015-08-25 16:27:15 -0700

    Allow glibc/linux to build without libldns
    This change allows the glibc/linux build to take advantage
    of the non-ldns path in dns_utils.c by making res_setservers
    optional (this is not available in glibc) and automatically
    detecting the need to -lresolv.

--- libpkg/pkg_config.c
+++ libpkg/pkg_config.c
@@ -1086,8 +1086,12 @@ pkg_ini(const char *path, const char *reposdir, pkg_init_flags flags)
 
 	/* bypass resolv.conf with specified NAMESERVER if any */
 	nsname = pkg_object_string(pkg_config_get("NAMESERVER"));
-	if (nsname != NULL)
-		set_nameserver(ucl_object_tostring_forced(o));
+	if (nsname != NULL) {
+		if (set_nameserver(ucl_object_tostring_forced(o)) != 0) {
+			pkg_emit_error("Unable to set nameserver");
+			return (EPKG_FATAL);
+		}
+	}
 
 	return (EPKG_OK);
 }
