5e5d4bb9d30a24346605a269090b8402a2872021
Baptiste Daroussin <bapt@FreeBSD.org>
2015-09-20 13:28:23 +0200

    Add a regression test about what jpeg->jpeg-turbo is supposed to do.
    Actually in the case it perfectly works, will be extended later with the
    failing case if I manage to reproduce it

---
+++ tests/frontend/jpeg.sh
@@ -0,0 +1,89 @@
+#! /usr/bin/env atf-sh
+
+. $(atf_get_srcdir)/test_environment.sh
+
+tests_init \
+	jpeg
+
+jpeg_body() {
+	touch libjpeg.so
+
+	cat << EOF > jpeg.ucl
+name: jpeg
+origin: graphics/jpeg
+version: "1.0"
+maintainer: test
+categories: [test]
+comment: jpeg
+www: http://jpeg
+prefix: /usr/local
+desc: jpeg desc
+files: {
+	${TMPDIR}/libjpeg.so: "",
+}
+EOF
+
+	cat << EOF > jpeg-turbo.ucl
+name: jpeg-turbo
+origin: graphics/jpeg-turbo
+version: "1.0"
+maintainer: test
+categories: [test]
+comment: jpeg
+www: http://jpeg
+prefix: /usr/local
+desc: jpeg desc
+files: {
+	${TMPDIR}/libjpeg.so: "",
+}
+EOF
+
+	cat << EOF > deponjpeg.ucl
+name: deponjpeg
+origin: graphics/deponjpeg
+version: "1.0"
+maintainer: test
+categories: [test]
+comment: jpeg
+www: http://jpeg
+prefix: /usr/local
+desc: jpeg desc
+deps: {
+	jpeg: { origin: graphics/jpeg, version: "1.0" }
+}
+EOF
+
+	cat << EOF > deponjpeg2.ucl
+name: deponjpeg
+origin: graphics/deponjpeg
+version: "1.0"
+maintainer: test
+categories: [test]
+comment: jpeg
+www: http://jpeg
+prefix: /usr/local
+desc: jpeg desc
+deps: {
+	jpeg-turbo: { origin: graphics/jpeg-turbo, version: "1.0" }
+}
+EOF
+
+	atf_check -o ignore -e empty pkg register -M jpeg.ucl
+	atf_check -o ignore -e empty pkg register -M deponjpeg.ucl
+
+	for p in jpeg deponjpeg2 jpeg-turbo; do
+		atf_check -o ignore \
+			-e empty \
+			pkg create -M ./${p}.ucl
+	done
+
+	atf_check -o ignore pkg repo .
+
+	cat << EOF > repo.conf
+local: {
+	url: file://${TMPDIR}/,
+	enabled: true
+}
+EOF
+	atf_check -o ignore -e empty -s exit:0 pkg -o REPOS_DIR="${TMPDIR}" -o PKG_CACHEDIR="${TMPDIR}" upgrade -y
+}
