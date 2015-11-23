9c17591776b5da218db8a5cc4f10aa4cfc20a9b8
Baptiste Daroussin <bapt@FreeBSD.org>
2015-11-23 15:08:04 +0100

    Use atf_skip on tests when not supported

--- tests/frontend/001sanity.sh
+++ tests/frontend/001sanity.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env atf-sh
 
 . $(atf_get_srcdir)/test_environment.sh
-if [ "${OS}" != "Darwin" ]; then
-	ldd=ldd
-fi
 tests_init \
 	which \
-	${ldd}
+	ldd
 
 which_body() {
 	atf_check \
@@ -16,8 +13,9 @@ which_body() {
 	    which pkg
 }
 
-
 ldd_body() {
+	atf_skip_on Darwin irrelevant on OSX
+
 	atf_check \
 	    -o match:".*libpkg.so.3 => $(atf_get_srcdir).*$" \
 	    -e empty \
