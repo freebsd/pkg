b4ea4b680e3bc44b20b271bebc1d313b649d19a9
Baptiste Daroussin <bapt@FreeBSD.org>
2016-04-05 09:04:15 +0200

    Add a test about empty configuration files

--- tests/frontend/config.sh
+++ tests/frontend/config.sh
@@ -2,7 +2,8 @@
 
 . $(atf_get_srcdir)/test_environment.sh
 tests_init \
-	inline_repo
+	inline_repo \
+	empty_conf \
 
 inline_repo_body() {
 	cat > pkgconfiguration << EOF
@@ -14,4 +15,34 @@ EOF
 	atf_check -o match:'^    url             : "file:///tmp",$' \
 		-o match:'^    url             : "file:///tmp2",$' \
 		pkg -o REPOS_DIR=/dev/null -C pkgconfiguration -vv
+}
+
+empty_conf_body() {
+	touch pkg.conf
+
+	cat << EOF > test.ucl
+name: test
+origin: test
+version: 1
+maintainer: test
+categories: [test]
+comment: a test
+www: http://test
+prefix: /
+desc: <<EOD
+Yet another test
+EOD
+EOF
+
+	atf_check \
+		-e empty \
+		-o ignore \
+		-s exit:0 \
+		pkg register -M test.ucl
+
+	atf_check \
+		-o ignore \
+		-e empty \
+		-s exit:0 \
+		pkg -C pkg.conf info test
 }
