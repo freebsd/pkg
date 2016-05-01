aa13a931b28250d855d5fe16a565b610bbe2c227
Baptiste Daroussin <bapt@FreeBSD.org>
2016-05-02 00:23:28 +0200

    Add O_CLOEXEC when duplicating the rootfd
    Reported by:	jilles

--- libpkg/pkg.c
+++ libpkg/pkg.c
@@ -1793,7 +1793,7 @@ pkg_open_root_fd(struct pkg *pkg)
 
 	path = pkg_kv_get(&pkg->annotations, "relocated");
 	if (path == NULL) {
-		if ((pkg->rootfd = dup(rootfd)) == -1) {
+		if ((pkg->rootfd = fcntl(rootfd, F_DUPFD_CLOEXEC, 0)) == -1) {
 			pkg_emit_errno("dup2", "rootfd");
 			return (EPKG_FATAL);
 		}
