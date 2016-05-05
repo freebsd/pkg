17216694a562dd9186054e62585e3111eb928541
Baptiste Daroussin <bapt@FreeBSD.org>
2016-05-05 19:01:55 +0200

    pkg delete pkg now fails instead of simply skip pkg itself (except -f is passed (fixes #1419)

--- libpkg/pkg_jobs.c
+++ libpkg/pkg_jobs.c
@@ -1998,7 +1998,8 @@ pkg_jobs_execute(struct pkg_jobs *j)
 			    strcmp(p->name, "pkg-devel") == 0) &&
 			    (flags & PKG_DELETE_FORCE) == 0) {
 				pkg_emit_error("Cannot delete pkg itself without force flag");
-				continue;
+				retcode = EPKG_FATAL;
+				goto cleanup;
 			}
 			/*
 			 * Assume that in upgrade we can remove packages with rdeps as
