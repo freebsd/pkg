df8a7e27dc2dded54720c35fa9c5ac693baefffb
Baptiste Daroussin <bapt@FreeBSD.org>
2016-11-07 01:05:47 +0100

    Fix regression tests

--- tests/frontend/rubypuppet.sh
+++ tests/frontend/rubypuppet.sh
@@ -346,8 +346,8 @@ Installed packages to be UPGRADED:
 	ruby: 2.0 -> 2.1
 
 Installed packages to be REINSTALLED:
-	rubygem-ruby-augeas-1.0 (direct dependency changed: ruby21-gems)
-	rubygem-hiera-1.0 (direct dependency changed: ruby21-gems)
+	rubygem-ruby-augeas-1.0 (direct dependency changed: ruby)
+	rubygem-hiera-1.0 (direct dependency changed: ruby)
 	puppet-1.0 (direct dependency changed: ruby)
 
 Number of packages to be removed: 1
