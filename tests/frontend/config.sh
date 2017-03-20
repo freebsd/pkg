84dd0ee323d335d0f716e27a2e9cc72e8d29de85
Brad Davis <brd@FreeBSD.org>
2017-03-20 05:56:59 -0600

    Sort tests

--- tests/frontend/config.sh
+++ tests/frontend/config.sh
@@ -2,21 +2,9 @@
 
 . $(atf_get_srcdir)/test_environment.sh
 tests_init \
-	inline_repo \
 	empty_conf \
+	inline_repo \
 	nameserver
-
-inline_repo_body() {
-	cat > pkgconfiguration << EOF
-repositories: {
-	pkg1: { url = file:///tmp },
-	pkg2: { url = file:///tmp2 },
-}
-EOF
-	atf_check -o match:'^    url             : "file:///tmp",$' \
-		-o match:'^    url             : "file:///tmp2",$' \
-		pkg -o REPOS_DIR=/dev/null -C pkgconfiguration -vv
-}
 
 empty_conf_body() {
 	touch pkg.conf
@@ -46,6 +34,18 @@ EOF
 		-e empty \
 		-s exit:0 \
 		pkg -C pkg.conf info test
+}
+
+inline_repo_body() {
+	cat > pkgconfiguration << EOF
+repositories: {
+	pkg1: { url = file:///tmp },
+	pkg2: { url = file:///tmp2 },
+}
+EOF
+	atf_check -o match:'^    url             : "file:///tmp",$' \
+		-o match:'^    url             : "file:///tmp2",$' \
+		pkg -o REPOS_DIR=/dev/null -C pkgconfiguration -vv
 }
 
 nameserver_body()
