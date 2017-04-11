497f2e690ca2777424f0a5cdc6b5796bdac281c2
Baptiste Daroussin <bapt@FreeBSD.org>
2017-04-11 21:16:25 +0200

    More FD usage

--- libpkg/pkgdb.c
+++ libpkg/pkgdb.c
@@ -58,9 +58,7 @@
 
 #include <sqlite3.h>
 
-#ifdef HAVE_SYS_STATFS_H
-#include <sys/statfs.h>
-#elif defined(HAVE_SYS_STATVFS_H)
+#if defined(HAVE_SYS_STATVFS_H)
 #include <sys/statvfs.h>
 #endif
 
@@ -1039,7 +1037,7 @@ pkgdb_syscall_overload(void)
 void
 pkgdb_setup_lock(void)
 {
-	const char *dbdir = pkg_object_string(pkg_config_get("PKG_DBDIR"));
+	int dbdirfd = pkg_get_dbdirfd();
 
 	if (pkg_object_bool(pkg_config_get("NFS_WITH_PROPER_LOCKING")))
 		return;
@@ -1051,14 +1049,14 @@ pkgdb_setup_lock(void)
 #if defined(HAVE_SYS_STATVFS_H) && defined(ST_LOCAL)
 	struct statvfs stfs;
 
-	if (statvfs(dbdir, &stfs) == 0) {
+	if (fstatvfs(dbdirfd, &stfs) == 0) {
 		if ((stfs.f_flag & ST_LOCAL) != ST_LOCAL)
 			sqlite3_vfs_register(sqlite3_vfs_find("unix-dotfile"), 1);
 	}
-#elif defined(HAVE_STATFS) && defined(MNT_LOCAL)
+#elif defined(HAVE_FSTATFS) && defined(MNT_LOCAL)
 	struct statfs stfs;
 
-	if (statfs(dbdir, &stfs) == 0) {
+	if (fstatfs(dbdirfd, &stfs) == 0) {
 		if ((stfs.f_flags & MNT_LOCAL) != MNT_LOCAL)
 			sqlite3_vfs_register(sqlite3_vfs_find("unix-dotfile"), 1);
 	}
