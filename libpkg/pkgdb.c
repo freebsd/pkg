0fa9907b94db544e4ec4e6a9175ff77abe5b942c
Baptiste Daroussin <bapt@FreeBSD.org>
2017-07-31 23:01:33 +0200

    Fake local.sqlite being in / to avoid sqlite to run getcwd
    Fixes: #1552

--- libpkg/pkgdb.c
+++ libpkg/pkgdb.c
@@ -1122,7 +1122,7 @@ retry:
 		pkgdb_setup_lock();
 		pkgdb_syscall_overload();
 
-		if (sqlite3_open("local.sqlite", &db->sqlite) != SQLITE_OK) {
+		if (sqlite3_open("/local.sqlite", &db->sqlite) != SQLITE_OK) {
 			ERROR_SQLITE(db->sqlite, "sqlite open");
 			pkgdb_close(db);
 			return (EPKG_FATAL);
