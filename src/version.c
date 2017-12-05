d2f64186a73cca68e5e4e7d0baa35f16f895d3f3
Baptiste Daroussin <bapt@FreeBSD.org>
2017-12-05 17:12:35 +0100

    Only parse the output of exec_buf if the command succeeded
    While here check the output of strrchr before using it

--- src/version.c
+++ src/version.c
@@ -718,11 +718,13 @@ port_version(UT_string *cmd, const char *portsdir, const char *origin,
 		argv[3] = "flavors-package-names";
 		argv[4] = NULL;
 
-		if (exec_buf(cmd, argv) != 0) {
+		if (exec_buf(cmd, argv) > 0) {
 			output = utstring_body(cmd);
 			while ((walk = strsep(&output, "\n")) != NULL) {
 				name = walk;
 				walk = strrchr(walk, '-');
+				if (walk == NULL)
+					continue;
 				walk[0] = '\0';
 				walk++;
 				if (strcmp(name, pkgname) == 0) {
