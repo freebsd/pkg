f69e38c5b38a547f64febdcd1c21f80426c753aa
Baptiste Daroussin <bapt@FreeBSD.org>
2018-12-28 16:48:43 +0100

    Somehow gmake does not seems to set "set -e" in shells executed for a target

--- mk/dir.mk
+++ mk/dir.mk
@@ -1,14 +1,14 @@
 all:
-	@for d in $(DIRS); do \
+	@set -e ; for d in $(DIRS); do \
 		$(MAKE) -C $$d ; \
 	done
 
 install:
-	@for d in $(DIRS); do \
+	@set -e ; for d in $(DIRS); do \
 		$(MAKE) -C $$d install ; \
 	done
 
 clean:
-	@for d in $(DIRS); do \
+	@set -e; for d in $(DIRS); do \
 		$(MAKE) -C $$d clean ; \
 	done
