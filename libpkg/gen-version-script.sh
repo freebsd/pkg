841c436eaa8abeb6829657065646cba1b0bebc39
Baptiste Daroussin <bapt@FreeBSD.org>
2019-03-27 13:41:24 +0100

    Fix issue with recent lld by adding symbols from libcsu

--- libpkg/gen-version-script.sh
+++ libpkg/gen-version-script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exctags -f /dev/stdout --c-kinds=fp pkg.h | awk 'BEGIN { print "LIBPKG_1.4 {"; print "global:" } /^[^!]/ { print "\t"$1";" } END { print "local:\n\t*;\n};" }' > libpkg.ver
+exctags -f /dev/stdout --c-kinds=fp pkg.h | awk 'BEGIN { print "LIBPKG_1.4 {"; print "global:" } /^[^!]/ { print "\t"$1";" } END { print "# Symbols from libcsu\n\t__progname;\n\tenviron;\nlocal:\n\t*;\n};" }' > libpkg.ver
 
