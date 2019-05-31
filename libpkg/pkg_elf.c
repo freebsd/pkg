f367891f13860cfb1543f006915370441dd5192e
Baptiste Daroussin <bapt@FreeBSD.org>
2019-05-31 15:22:55 +0200

    Fix ABI genaration on DragonFly
    Submitted by:	zrj <rimvydas.jasinskas@gmail.com>

--- libpkg/pkg_elf.c
+++ libpkg/pkg_elf.c
@@ -799,7 +799,7 @@ elf_note_analyse(Elf_Data *data, GElf_Ehdr *elfhdr, struct elf_info *ei)
 		if (ei->osversion != NULL)
 			*ei->osversion = version;
 #if defined(__DragonFly__)
-		xasprintf(&ei->strversion, "%d.%d", (((version / 100 % 1000)+1)/2)*2);
+		xasprintf(&ei->strversion, "%d.%d", version / 100000, (((version / 100 % 1000)+1)/2)*2);
 #elif defined(__NetBSD__)
 		xasprintf(&ei->strversion, "%d", (version + 1000000) / 100000000);
 #else
