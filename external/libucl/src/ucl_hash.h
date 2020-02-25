6aea7863884802b51e3053eb524fa98ad947a0a9
Baptiste Daroussin <bapt@FreeBSD.org>
2020-02-25 11:51:47 +0100

    Update to latest libucl from github

--- external/libucl/src/ucl_hash.h
+++ external/libucl/src/ucl_hash.h
@@ -103,5 +103,7 @@ bool ucl_hash_iter_has_next (ucl_hash_t *hashlin, ucl_hash_iter_t iter);
  * @param hashlin
  */
 bool ucl_hash_reserve (ucl_hash_t *hashlin, size_t sz);
+void ucl_hash_sort (ucl_hash_t *hashlin, enum ucl_object_keys_sort_flags fl);
+
 
 #endif
