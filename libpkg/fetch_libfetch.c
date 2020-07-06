06555ff9393e8210918adb510a16eb25b0ce4eb7
Baptiste Daroussin <bapt@FreeBSD.org>
2020-07-06 10:21:25 +0200

    Remove unneccessary line from license

--- libpkg/fetch_libfetch.c
+++ libpkg/fetch_libfetch.c
@@ -1,6 +1,5 @@
 /*-
  * Copyright (c) 2020 Baptiste Daroussin <bapt@FreeBSD.org>
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
