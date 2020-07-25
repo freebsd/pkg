b69537c73203f6d39788a1c1fea71d3cefced4ab
Tobias Kortkamp <t@tobik.me>
2020-07-25 15:06:45 +0200

    Unbreak build with -fno-common
    -fno-common is the default in GCC10 and will also be the default
    in Clang11.
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at alias.c
    >>>            alias.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at alias.c
    >>>            alias.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at alias.c
    >>>            alias.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at annotate.c
    >>>            annotate.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at annotate.c
    >>>            annotate.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at annotate.c
    >>>            annotate.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at audit.c
    >>>            audit.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at audit.c
    >>>            audit.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at audit.c
    >>>            audit.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at autoremove.c
    >>>            autoremove.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at autoremove.c
    >>>            autoremove.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at autoremove.c
    >>>            autoremove.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at backup.c
    >>>            backup.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at backup.c
    >>>            backup.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at backup.c
    >>>            backup.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at check.c
    >>>            check.o:(.bss+0x4)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at check.c
    >>>            check.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbtodl
    >>> defined at add.c
    >>>            add.o:(nbtodl)
    >>> defined at check.c
    >>>            check.o:(.bss+0x8)
    
    ld: error: duplicate symbol: nbactions
    >>> defined at add.c
    >>>            add.o:(nbactions)
    >>> defined at clean.c
    >>>            clean.o:(.bss+0x0)
    
    ld: error: duplicate symbol: nbdone
    >>> defined at add.c
    >>>            add.o:(nbdone)
    >>> defined at clean.c
    >>>            clean.o:(.bss+0x4)
    
    Signed-off-by: Tobias Kortkamp <t@tobik.me>

--- src/globals.c
+++ src/globals.c
@@ -31,6 +31,9 @@ int case_sensitive; /* Case sensitive queries */
 int force; /* Forced operation */
 int quiet; /* Silent output */
 int newpkgversion; /* New package version is available */
+int nbactions;
+int nbdone;
+int nbtodl;
 
 void
 set_globals(void)
@@ -43,5 +46,8 @@ set_globals(void)
 	force = 0;
 	quiet = 0;
 	newpkgversion = 0;
+	nbactions = 0;
+	nbdone = 0;
+	nbtodl = 0;
 }
 
