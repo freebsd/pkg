b2032769223fe9f3ee0065e13d7e3470bc509b69
Kyle Evans <kevans@FreeBSD.org>
2021-01-14 11:30:30 -0600

    tests: frontend: add some version tests for special prefixes

--- tests/frontend/version.sh
+++ tests/frontend/version.sh
@@ -11,6 +11,12 @@ version_body() {
 	atf_check -o inline:">\n" -s exit:0 pkg version -t 2 1
 	atf_check -o inline:"=\n" -s exit:0 pkg version -t 2 2
 	atf_check -o inline:"<\n" -s exit:0 pkg version -t 2 1,1
+	# Special prefixes
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1.pl1 1.alpha1
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1.alpha1 1.beta1
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1.beta1 1.pre1
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1.pre1 1.rc1
+	atf_check -o inline:"<\n" -s exit:0 pkg version -t 1.rc1 1
 }
 
 compare_body() {
