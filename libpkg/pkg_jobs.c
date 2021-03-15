4d5864d44ba5d182c16aa4865cded7394fdc725d
Baptiste Daroussin <bapt@FreeBSD.org>
2021-03-15 18:05:08 +0100

    install: lookup for .bsd files as well

--- libpkg/pkg_jobs.c
+++ libpkg/pkg_jobs.c
@@ -201,11 +201,12 @@ pkg_jobs_maybe_match_file(struct job_pattern *jp, const char *pattern)
 		 * Compare suffix with .txz or .tbz
 		 */
 		dot_pos ++;
-		if (strcmp(dot_pos, "tzst") == 0 ||
-			strcmp(dot_pos, "txz") == 0 ||
-			strcmp(dot_pos, "tbz") == 0 ||
-			strcmp(dot_pos, "tgz") == 0 ||
-			strcmp(dot_pos, "tar") == 0) {
+		if (strcmp(dot_pos, "bsd") == 0 ||
+		    strcmp(dot_pos, "tzst") == 0 ||
+		    strcmp(dot_pos, "txz") == 0 ||
+		    strcmp(dot_pos, "tbz") == 0 ||
+		    strcmp(dot_pos, "tgz") == 0 ||
+		    strcmp(dot_pos, "tar") == 0) {
 			if ((pkg_path = realpath(pattern, NULL)) != NULL) {
 				/* Dot pos is one character after the dot */
 				int len = dot_pos - pattern;
