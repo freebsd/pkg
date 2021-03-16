4e913f134bcf0a13b9e1f172aacc320f8b51c31a
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-03-16 17:59:54 +0000

    Allow running abi:basic on non-FreeBSD

--- tests/frontend/abi.sh
+++ tests/frontend/abi.sh
@@ -5,11 +5,17 @@ tests_init \
 	basic
 
 basic_body() {
-	if [ `uname -s` != "FreeBSD" ]; then
-		atf_skip "Not yet supported on non FreeBSD"
+	_uname_s="$(uname -s)"
+	_expected="TODO: implement me"
+	if [ "${_uname_s}" = "FreeBSD" ]; then
+		_expected="FreeBSD:13:amd64\n"
+	else
+		# The FreeBSD ELF ABI_FILE should be ignored on other systems:
+		_expected="${_uname_s}:$(uname -r | cut -d. -f1):$(uname -p)\n"
+		# atf_skip "Not yet supported on ${_uname_s}"
 	fi
 	atf_check \
-		-o inline:"FreeBSD:13:amd64\n" \
+		-o inline:"${_expected}" \
 		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/fbsd.bin config abi
 
 #	atf_check \
