ee29d2fa44e81b06fd031df01a4de64a7eaf6189
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-03-16 17:47:01 +0000

    Fix abi:basic after latest commits
    Fixes: 1278736cb24ed1f1d25c5b843ea2b803e4ec03e0

--- tests/frontend/abi.sh
+++ tests/frontend/abi.sh
@@ -10,7 +10,7 @@ basic_body() {
 	fi
 	atf_check \
 		-o inline:"FreeBSD:13:amd64\n" \
-		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/.pkg.bin config abi
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/fbsd.bin config abi
 
 #	atf_check \
 #		-o inline:"dragonfly:5.10:x86:64\n" \
