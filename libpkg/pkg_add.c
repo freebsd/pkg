913cf560a499e1a79eec9fddfccd9b4fd568cf14
Franco Fichtner <franco@opnsense.org>
2021-08-18 10:20:58 +0200

    pkg-add: provide fallback for version mismatch
    Currenty either we find the proper version or we go to the
    nearest match, but not both, which leads to missing errors
    when there is no need for it.

--- libpkg/pkg_add.c
+++ libpkg/pkg_add.c
@@ -871,7 +871,7 @@ pkg_add_check_pkg_archive(struct pkgdb *db, struct pkg *pkg,
 	int	ret, retcode;
 	struct pkg_dep	*dep = NULL;
 	char	bd[MAXPATHLEN], *basedir = NULL;
-	char	dpath[MAXPATHLEN], *ppath;
+	char	dpath[MAXPATHLEN] = "", *ppath;
 	const char	*ext = NULL;
 	struct pkg	*pkg_inst = NULL;
 	bool	fromstdin;
@@ -943,7 +943,9 @@ pkg_add_check_pkg_archive(struct pkgdb *db, struct pkg *pkg,
 		if (dep->version != NULL && dep->version[0] != '\0') {
 			snprintf(dpath, sizeof(dpath), "%s/%s-%s%s", bd,
 				dep->name, dep->version, ext);
-		} else {
+		}
+
+		if (strlen(dpath) == 0 || access(dpath, F_OK) != 0) {
 			snprintf(dpath, sizeof(dpath), "%s/%s-*%s", bd,
 			    dep->name, ext);
 			ppath = pkg_globmatch(dpath, dep->name);
