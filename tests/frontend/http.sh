2338ea537b7819e207062355380e9bc5c9cb7a77
John Hood <cgull@glup.org>
2021-12-03 07:22:36 -0500

    Improve http tests-- parallel jobs & better portability
    Rename http:simple_fetch (which tests 'pkg update') to
    http:simple_update
    
    Add a new http:simple_fetch (which actually tests 'pkg fetch') .  For
    issue #2011.
    
    Use python3's bundled http.server instead of minihttpd-- that's more
    universally available, and it does dynamic port selection.  This
    allows testing on MacOS, Ubuntu, Debian.
    
    Rework tests to run correctly in parallel-- previously they could fail
    when a second (third?) instance of mini_httpd was run on the same port
    
    .cirrus.yml: Change build deps, needed for new/expanded testing
    
    Drop mini_httpd
    
    Add python3, for http server, on all platforms
    
    Add bzip2 packages on FreeBSD, Debian, and Fedora.  MacOS already bundles it

--- tests/frontend/http.sh
+++ tests/frontend/http.sh
@@ -2,35 +2,63 @@
 
 . $(atf_get_srcdir)/test_environment.sh
 
-tests_init \
-	simple_repo \
-	simple_fetch
+CLEANUP="simple_fetch simple_repo simple_audit"
 
-cleanup() {
-	pkill -15 -F ${TMPDIR}/http.pid
+tests_init \
+	simple_fetch \
+	simple_repo \
+	simple_audit
+
+
+httpd_startup() {
+	pidfile=${TMPDIR}/http.pid
+	statusfile=${TMPDIR}/http.status
+	: > ${pidfile}
+	: > ${statusfile}
+
+	DIR=$1
+	python3 -u -m http.server -d "${DIR}" --bind 127.0.0.1 0 > ${statusfile} &
+	jobs -p %1 > ${pidfile}
+	# a crude way to wait for its startup, I can't think of anything better
+	while kill -s 0 %1 && ! [ -s ${statusfile} ]; do
+	    sleep .1
+	done
+        url=$(sed -n 's#.*(\(http://.*\)/).*#\1#p' ${statusfile})
+	atf_check test -n "${url}"
+}
+
+httpd_cleanup() {
+	pid=$(cat ${TMPDIR}/http.pid)
+	if [ -z "${pid}" ]; then
+	    return 1
+	fi
+	if kill -s 0 ${pid}; then
+	    kill -s INT ${pid}
+	    for i in 0 1 2 3 4 5 6 7 8 9; do
+		if ! kill -s 0 ${pid}; then
+		    break
+		fi
+	    done
+	fi
 }
 
 simple_repo_body()
 {
-	pidfile=${TMPDIR}/http.pid
-	touch ${pidfile}
-
-	atf_require mini_httpd "Requires mini_httpd to run this test"
+	atf_require python3 "Requires python3 to run this test"
 	atf_check sh ${RESOURCEDIR}/test_subr.sh new_pkg test test 1
 	atf_check pkg create -M test.ucl
 	mkdir repo
 	mv test-1.txz test-1.pkg repo/
 	atf_check -o ignore pkg repo repo
 
-	trap cleanup EXIT
-	atf_check -o empty -e empty mini_httpd -h 127.0.0.1 -p 64242 -d ${TMPDIR}/repo -dd ${TMPDIR}/repo -i ${pidfile}
+	httpd_startup ${TMPDIR}/repo
 
 	cat > pkg.conf << EOF
 PKG_DBDIR=${TMPDIR}
 PKG_CACHEDIR=${TMPDIR}/cache
 REPOS_DIR=[]
 repositories: {
-	local: { url: http://localhost:64242 }
+	local: { url: ${url} }
 }
 EOF
 	atf_check -o ignore \
@@ -39,12 +67,44 @@ EOF
 		pkg -C ./pkg.conf install -y test
 }
 
+simple_repo_cleanup()
+{
+    httpd_cleanup
+}
+
 simple_fetch_body()
 {
-	pidfile=${TMPDIR}/http.pid
-	touch ${pidfile}
+	atf_require python3 "Requires python3 to run this test"
+	atf_check sh ${RESOURCEDIR}/test_subr.sh new_pkg test test 1
+	atf_check pkg create -M test.ucl
+	mkdir repo
+	mv test-1.txz test-1.pkg repo/
+	atf_check -o ignore pkg repo repo
 
-	atf_require mini_httpd "Requires mini_httpd to run this test"
+	httpd_startup ${TMPDIR}/repo
+
+	cat > pkg.conf << EOF
+PKG_DBDIR=${TMPDIR}
+PKG_CACHEDIR=${TMPDIR}/cache
+REPOS_DIR=[]
+repositories: {
+	local: { url: ${url} }
+}
+EOF
+	atf_check -o ignore \
+		pkg -C ./pkg.conf update
+	atf_check -o match:"test-1" \
+		pkg -C ./pkg.conf fetch -y test
+}
+
+simple_fetch_cleanup()
+{
+    httpd_cleanup
+}
+
+simple_audit_body()
+{
+	atf_require python3 "Requires python3 to run this test"
 
 	mkdir rootdir
 	cat > ${TMPDIR}/rootdir/meh <<EOF
@@ -54,14 +114,17 @@ simple_fetch_body()
 EOF
 	bzip2 ${TMPDIR}/rootdir/meh
 
-	trap cleanup EXIT
-	atf_check -o empty -e empty mini_httpd -h 127.0.0.1 -p 64242 -d ${TMPDIR}/rootdir -dd ${TMPDIR}/rootdir -i ${pidfile}
+	httpd_startup ${TMPDIR}/rootdir
 
 	cat > pkg.conf << EOF
 PKG_DBDIR=${TMPDIR}
-VULNXML_SITE = "http://localhost:64242/meh.bz2"
+VULNXML_SITE = "${url}/meh.bz2"
 EOF
 
 	atf_check -o ignore pkg -C ./pkg.conf audit -F
 
+}
+simple_audit_cleanup()
+{
+    httpd_cleanup
 }
