206ecf7bf6884f2d7e5c1e821f0b7eb80888e37f
Baptiste Daroussin <bapt@FreeBSD.org>
2022-01-21 13:57:27 +0100

    fix bad syntax for cpp instructions

--- tests/lib/packing.c
+++ tests/lib/packing.c
@@ -78,7 +78,7 @@ ATF_TC_BODY(packing_set_format, tc)
 	struct archive *a = archive_write_new();
 	ATF_CHECK(a != NULL);
 
-#ifdef HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD && __FreeBSD_version >= 1300000
+#if defined(HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD) && __FreeBSD_version >= 1300000
 	ATF_REQUIRE_STREQ(packing_set_format(a, TZS, -1), "tzst");
 #endif
 	ATF_REQUIRE_STREQ(packing_set_format(a, TXZ, -1), "txz");
@@ -88,7 +88,7 @@ ATF_TC_BODY(packing_set_format, tc)
 	ATF_REQUIRE_EQ(packing_set_format(a, 28, -1), NULL);
 
 	/* compression min */
-#ifdef HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD && __FreeBSD_version >= 1300000
+#if defined(HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD) && __FreeBSD_version >= 1300000
 	ATF_REQUIRE_STREQ(packing_set_format(a, TZS, INT_MIN), "tzst");
 #endif
 	ATF_REQUIRE_STREQ(packing_set_format(a, TXZ, INT_MIN), "txz");
@@ -98,7 +98,7 @@ ATF_TC_BODY(packing_set_format, tc)
 	ATF_REQUIRE_EQ(packing_set_format(a, 28, INT_MIN), NULL);
 
 	/* compression max */
-#ifdef HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD && __FreeBSD_version >= 1300000
+#if defined(HAVE_ARCHIVE_WRITE_ADD_FILTER_ZSTD) && __FreeBSD_version >= 1300000
 	ATF_REQUIRE_STREQ(packing_set_format(a, TZS, INT_MAX), "tzst");
 #endif
 	ATF_REQUIRE_STREQ(packing_set_format(a, TXZ, INT_MAX), "txz");
