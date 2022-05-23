5edd062df23d708c0dc7c2b58a07de9b38b92a98
Baptiste Daroussin <bapt@FreeBSD.org>
2022-05-23 10:39:19 +0200

    pkgdb: remove wrong concatenation for licenses

--- libpkg/pkgdb_iterator.c
+++ libpkg/pkgdb_iterator.c
@@ -535,7 +535,7 @@ static int
 pkgdb_load_license(sqlite3 *sqlite, struct pkg *pkg)
 {
 	const char	 sql[] = ""
-		"SELECT ifnull(group_concat(name, ', '), '') AS name"
+		"SELECT name "
 		"  FROM pkg_licenses, licenses AS l"
 		"  WHERE package_id = ?1"
 		"    AND license_id = l.id"
