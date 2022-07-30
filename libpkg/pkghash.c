freebsd/pkg

commit 91a326bdf0fb41a3e6a89faec31e9c76b8c3ad5e

pkghash: Reduce the amount of memory allocated for the hash

libpkg/pkghash.c
@@ -141,7 +141,7 @@ pkghash_expand(pkghash *table)
 bool
 pkghash_add(pkghash *table, const char *key, void *value, void (*free_func)(void *))
 {
-	if (table->count >= table->capacity / 2 && !pkghash_expand(table))
+	if (table->count >= table->capacity -1 && !pkghash_expand(table))
 		return (NULL);
 
 	return (pkghash_set_entry(table->entries, table->capacity, key, value,

commit 2b60b68eb95fc2c3b80dd07bce15398fab33d997

Fill hashes to 50%, not 100%.
This fixes a performance regression introduced in 91a326bdf0fb41a3e6a89faec31e9c76b8c3ad5e.

libpkg/pkghash.c
@@ -141,7 +141,7 @@ pkghash_expand(pkghash *table)
 bool
 pkghash_add(pkghash *table, const char *key, void *value, void (*free_func)(void *))
 {
-	if (table->count >= table->capacity -1 && !pkghash_expand(table))
+	if (table->count * 2  >= table->capacity && !pkghash_expand(table))
 		return (NULL);
 
 	return (pkghash_set_entry(table->entries, table->capacity, key, value,