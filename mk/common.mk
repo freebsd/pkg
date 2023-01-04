98296a41fef68567cc2943b22a6f79a5d12457a0
Jessica Clarke <jrtc27@jrtc27.com>
2023-01-04 20:24:42 +0000

    Fix out-of-tree libelf, libmachista and tests builds
    These were missed in fbaaacce6c5d ("Support out-of-tree builds") since
    libelf is only used if a system one isn't present, libmachista is only
    used on Darwin and tests are only built if atf-c and kyua are available.

--- mk/common.mk
+++ mk/common.mk
@@ -6,9 +6,16 @@ CFLAGS+=	$(CPPFLAGS)
 CFLAGS+=	-Werror=implicit-function-declaration
 CFLAGS+=	-Werror=return-type
 
--include $(DEPFILES)
+# bmake's traditional include support treats empty strings in the expanded
+# result (whether because the variable is empty or there are consecutive
+# whitespace characters) as file names, and thus tries to read the containing
+# directory as a Makefile, which fails, and isn't ignored since it exists.
+# Work around this quirky behaviour by adding an extra entry that should never
+# exist and then normalize its whitespace during substitution with :=.
+DEPFILES_NONEMPTY=	$(DEPFILES) /nonexistent
+-include $(DEPFILES_NONEMPTY:=)
 
-.SUFFIXES: .pico .in
+.SUFFIXES: .pico .in .bin .binin
 
 .c.o:
 	$(CC) -Wall -Wextra -std=gnu99 -D_GNU_SOURCE=1 -MT $@ -MD -MP -MF $*.Tpo -o $@ -c $(CFLAGS) $(LOCAL_CFLAGS) $<
@@ -21,3 +28,6 @@ CFLAGS+=	-Werror=return-type
 .in:
 	sed -e 's|@prefix@|$(PREFIX)|g; s|@abs_top_srcdir@|$(top_srcdir)|g' \
 		-e 's|@VERSION@|$(version)|g' $< > $@
+
+.binin.bin:
+	cp $< $@
