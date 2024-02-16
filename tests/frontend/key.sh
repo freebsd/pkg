542057c403f3794444c0b18282f4253a6a9c1a28
Kyle Evans <kevans@FreeBSD.org>
2024-02-16 17:33:38 -0600

    tests: frontend: add some tests to demonstrate ECC functionality
    We augmented both the fingerprint and pubkey mechanisms to be able to do
    signatures; fingerprints can only work with ECDSA at the moment because it
    relies on an openssl signer, but pubkey can do both ECDSA and EdDSA.

--- tests/frontend/key.sh
+++ tests/frontend/key.sh
@@ -4,7 +4,8 @@
 
 tests_init \
 	key_create \
-	key_pubout
+	key_pubout \
+	key_sign
 
 key_create_head() {
 	atf_set "require.progs" "openssl"
@@ -12,16 +13,31 @@ key_create_head() {
 key_create_body() {
 	echo "secure msg" > msg
 
-	atf_check -o ignore -e ignore -x pkg key --create -t rsa repo
+	atf_check -o save:repo.pub -e ignore -x pkg key --create -t rsa \
+	    repo.key
+
 	# Group permissions are OK, but let's strive for limited to the user.
-	atf_check -o match:'-{6}$' -x \
-	    'ls -l repo | cut -c1-10'
-	# Should have also created the corresponding pub key.
-	atf_check test -f repo.pub
+	# This doesn't use stat(1) to side-step the differences between
+	# platforms in how to request specific fields; the ls(1) mode
+	# representation is usually consistent enough.
+	atf_check -o match:'-{6}$' -x 'ls -l repo.key | cut -c1-10'
+	# Should have also output the corresponding pub key.
+	atf_check test -s repo.pub
 
 	# Make sure it's functional.
-	atf_check -o save:msg.sign openssl dgst -sign repo -sha256 -binary msg
-	atf_check -o ignore openssl dgst -sha256 -verify repo.pub -signature msg.sign msg
+	atf_check -o save:msg.sign openssl dgst -sign repo.key -sha256 \
+	    -binary msg
+	atf_check -o ignore openssl dgst -sha256 -verify repo.pub \
+	    -signature msg.sign msg
+
+	for signer in ecc ecdsa eddsa; do
+		rm -f repo.key repo.pub
+		atf_check -o save:repo.pub -e ignore -x pkg key --create \
+		    -t "$signer" repo.key
+
+		atf_check -o match:'-{6}$' -x 'ls -l repo.key | cut -c1-10'
+		atf_check test -s repo.pub
+	done
 }
 
 key_pubout_head() {
@@ -41,4 +57,23 @@ key_pubout_body() {
 	atf_check -o ignore openssl dgst -sha256 -verify repo.pub -signature msg.sign msg
 }
 
+key_sign_head() {
+	atf_set "require.progs" "openssl"
+}
+key_sign_body() {
+	echo "secure msg" > msg
 
+	for signer in rsa ecdsa; do
+		rm -f repo.key repo.pub msg.sig
+
+		# Generate a key with pkg
+		atf_check -o save:repo.pub -e ignore \
+		    pkg key --create -t "$signer" repo.key
+
+		atf_check -o save:msg.sig \
+		    pkg key --sign -t "$signer" repo.key < msg
+
+		atf_check -o ignore openssl dgst -sha256 -verify repo.pub \
+		    -signature msg.sig msg
+	done
+}
