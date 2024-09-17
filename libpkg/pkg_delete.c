8e9174480b66aa2fdaaea624d311871c35c6897c
Gleb Popov <6yearold@gmail.com>
2024-09-17 11:38:14 +0300

    Fix heap-buffer-overflow found by ASAN
    The "pkg->prefix" might be an empty string. Tests do that for example.
    
    Sponsored by:	Future Crew, LLC

--- libpkg/pkg_delete.c
+++ libpkg/pkg_delete.c
@@ -268,7 +268,7 @@ pkg_effective_rmdir(struct pkgdb *db, struct pkg *pkg)
 {
 	char prefix_r[MAXPATHLEN];
 
-	snprintf(prefix_r, sizeof(prefix_r), "%s", pkg->prefix + 1);
+	snprintf(prefix_r, sizeof(prefix_r), "%s", pkg->prefix[0] ? pkg->prefix + 1 : "");
 	tll_foreach(pkg->dir_to_del, d)
 		rmdir_p(db, pkg, d->item, prefix_r);
 }
