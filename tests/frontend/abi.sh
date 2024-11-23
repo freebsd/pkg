freebsd/pkg

commit c2fe22b4f51bdd14763555ed273047c66878de5b

ABI: get rid of altabi
While we still expose ALTABI to the client for backward compatibility
and provide user the time to move to ABI, internally we now only work
on ABI

libpkg/pkg_elf.c
@@ -1,28 +1,8 @@
 /*-
- * Copyright (c) 2011-2012 Baptiste Daroussin <bapt@FreeBSD.org>
+ * Copyright (c) 2011-2024 Baptiste Daroussin <bapt@FreeBSD.org>
  * Copyright (c) 2012-2013 Matthew Seaman <matthew@FreeBSD.org>
- * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer
- *    in this position and unchanged.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #ifdef HAVE_CONFIG_H
@@ -166,7 +146,7 @@ shlib_valid_abi(const char *fpath, GElf_Ehdr *hdr)
 	 */
 	arch[0] = '\0';
 	wordsize[0] = '\0';
-	p = pkg_object_string(pkg_config_get("ALTABI"));
+	p = ctx.oi->abi;
 	for(semicolon = 0; semicolon < 3 && p != NULL; semicolon ++, p ++) {
 		p = strchr(p, ':');
 		if (p != NULL) {
@@ -742,7 +722,6 @@ elf_note_analyse(Elf_Data *data, GElf_Ehdr *elfhdr, struct os_info *oi)
 	return (true);
 }
 
-
 int
 pkg_get_myarch_elfparse(int fd, struct os_info *oi)
 {
@@ -753,11 +732,9 @@ pkg_get_myarch_elfparse(int fd, struct os_info *oi)
 	Elf_Scn *scn = NULL;
 	int ret = EPKG_OK;
 	const char *arch,*abi, *endian_corres_str, *wordsize_corres_str, *fpu;
-	char *dest = oi->altabi;
-	size_t sz = sizeof(oi->altabi);
+	char *dest = oi->abi;
+	size_t sz = sizeof(oi->abi);
 	size_t dsz;
-
-	arch = NULL;
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
 		pkg_emit_error("ELF library initialization failed: %s",
@@ -805,154 +782,15 @@ pkg_get_myarch_elfparse(int fd, struct os_info *oi)
 	wordsize_corres_str = elf_corres_to_string(wordsize_corres,
 	    (int)elfhdr.e_ident[EI_CLASS]);
 
-	arch = elf_corres_to_string(mach_corres, (int) elfhdr.e_machine);
+	if (oi->ostype == OS_FREEBSD && elfhdr.e_machine == EM_X86_64)
+		oi->arch = xstrdup("amd64");
+	else if (oi->ostype == OS_DRAGONFLY && elfhdr.e_machine == EM_X86_64)
+		oi->arch = xstrdup("x86:64");
+	else
+		oi->arch = xstrdup(elf_corres_to_string(mach_corres, (int) elfhdr.e_machine));
 
-	switch (elfhdr.e_machine) {
-	case EM_ARM:
-		endian_corres_str = elf_corres_to_string(endian_corres,
-		    (int)elfhdr.e_ident[EI_DATA]);
-
-		if (elfhdr.e_flags & EF_ARM_VFP_FLOAT)
-			fpu = "hardfp";
-		else
-			fpu = "softfp";
-
-		if ((elfhdr.e_flags & 0xFF000000) != 0) {
-			const char *sh_name = NULL;
-			size_t shstrndx;
-
-			/* This is an EABI file, the conformance level is set */
-			abi = "eabi";
-
-			/* Find which TARGET_ARCH we are building for. */
-			elf_getshdrstrndx(elf, &shstrndx);
-			while ((scn = elf_nextscn(elf, scn)) != NULL) {
-				sh_name = NULL;
-				if (gelf_getshdr(scn, &shdr) != &shdr) {
-					scn = NULL;
-					break;
-				}
-
-				sh_name = elf_strptr(elf, shstrndx,
-				    shdr.sh_name);
-				if (sh_name == NULL)
-					continue;
-				if (STREQ(".ARM.attributes", sh_name))
-					break;
-			}
-			if (scn != NULL && sh_name != NULL) {
-				data = elf_getdata(scn, NULL);
-				/*
-				 * Prior to FreeBSD 10.0 libelf would return
-				 * NULL from elf_getdata on the .ARM.attributes
-				 * section. As this was the first release to
-				 * get armv6 support assume a NULL value means
-				 * arm.
-				 *
-				 * This assumption can be removed when 9.x
-				 * is unsupported.
-				 */
-				if (data != NULL) {
-					arch = aeabi_parse_arm_attributes(
-					    data->d_buf, data->d_size);
-					if (arch == NULL) {
-						ret = EPKG_FATAL;
-						pkg_emit_error(
-						    "unknown ARM ARCH");
-						goto cleanup;
-					}
-				}
-			} else {
-				ret = EPKG_FATAL;
-				pkg_emit_error("Unable to find the "
-				    ".ARM.attributes section");
-				goto cleanup;
-			}
-
-		} else if (elfhdr.e_ident[EI_OSABI] != ELFOSABI_NONE) {
-			/*
-			 * EABI executables all have this field set to
-			 * ELFOSABI_NONE, therefore it must be an oabi file.
-			 */
-			abi = "oabi";
-                } else {
-			/*
-			 * We may have failed to positively detect the ABI,
-			 * set the ABI to unknown. If we end up here one of
-			 * the above cases should be fixed for the binary.
-			 */
-			ret = EPKG_FATAL;
-			pkg_emit_error("unknown ARM ABI");
-			goto cleanup;
-		}
-		dsz = strlen(dest);
-		snprintf(dest + dsz, sz - dsz,
-		    ":%s:%s:%s:%s:%s", arch, wordsize_corres_str,
-		    endian_corres_str, abi, fpu);
-		break;
-	case EM_MIPS:
-		/*
-		 * this is taken from binutils sources:
-		 * include/elf/mips.h
-		 * mapping is figured out from binutils:
-		 * gas/config/tc-mips.c
-		 */
-		switch (elfhdr.e_flags & EF_MIPS_ABI) {
-			case E_MIPS_ABI_O32:
-				abi = "o32";
-				break;
-			case E_MIPS_ABI_N32:
-				abi = "n32";
-				break;
-			default:
-				if (elfhdr.e_ident[EI_DATA] == ELFCLASS32)
-					abi = "o32";
-				else if (elfhdr.e_ident[EI_DATA] == ELFCLASS64)
-					abi = "n64";
-				else
-					abi = "unknown";
-				break;
-		}
-		endian_corres_str = elf_corres_to_string(endian_corres,
-		    (int)elfhdr.e_ident[EI_DATA]);
-
-		dsz = strlen(dest);
-		snprintf(dest + dsz, sz - dsz, ":%s:%s:%s:%s",
-		    arch, wordsize_corres_str, endian_corres_str, abi);
-		break;
-#if defined(EM_RISCV) && defined(EF_RISCV_FLOAT_ABI_MASK)
-	case EM_RISCV:
-		switch (elfhdr.e_flags & EF_RISCV_FLOAT_ABI_MASK) {
-			case EF_RISCV_FLOAT_ABI_SOFT:
-				abi = "sf";
-				break;
-			case EF_RISCV_FLOAT_ABI_DOUBLE:
-				abi = "hf";
-				break;
-			default:
-				abi = "unknown";
-				break;
-		}
-		dsz = strlen(dest);
-		snprintf(dest + dsz, sz - dsz, ":%s:%s:%s",
-		    arch, wordsize_corres_str, abi);
-		break;
-#endif
-	case EM_PPC:
-	case EM_PPC64:
-		endian_corres_str = elf_corres_to_string(endian_corres,
-		    (int)elfhdr.e_ident[EI_DATA]);
-
-		dsz = strlen(dest);
-		snprintf(dest + dsz, sz - dsz, ":%s:%s:%s",
-		    arch, wordsize_corres_str, endian_corres_str);
-		break;
-	default:
-		dsz = strlen(dest);
-		snprintf(dest + dsz, sz - dsz, ":%s:%s",
-		    arch, wordsize_corres_str);
-		break;
-	}
+	dsz = strlen(dest);
+	snprintf(dest + dsz, sz - dsz, ":%s", oi->arch);
 
 cleanup:
 	if (elf != NULL)

libpkg/private/elf_tables.h
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 2012 Olivier Houchard <cognet@FreeBSD.org>
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -11,7 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -31,8 +31,8 @@ struct _elf_corres {
 };
 
 static const struct _elf_corres mach_corres[] = {
-	{ EM_386, "x86" },
-	{ EM_X86_64, "x86" },
+	{ EM_386, "i386" },
+	{ EM_X86_64, "x86_64" },
 	{ EM_AARCH64, "aarch64" },
 	{ EM_ARM, "arm" },
 	{ EM_MIPS, "mips" },

tests/frontend/abi.sh
@@ -65,12 +65,12 @@ elfparse_body() {
 		-o inline:"${_expected}" \
 		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/dfly.bin config altabi
 
-	_expected="Linux:3.2:amd64\n"
+	_expected="Linux:3.2:x86_64\n"
 	atf_check \
 		-o inline:"${_expected}" \
 		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/linux.bin config abi
 
-	_expected="linux:3.2:x86:64\n"
+	_expected="linux:3.2:x86_64\n"
 	atf_check \
 		-o inline:"${_expected}" \
 		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/linux.bin config altabi

libpkg/pkg_abi.c
@@ -135,40 +135,16 @@ pkg_get_myarch_with_legacy(struct os_info *oi)
 		free(oi->name);
 		return (err);
 	}
-	strlcpy(oi->abi, oi->altabi, sizeof(oi->abi));
 
-    for(char *p = oi->altabi; *p; ++p) {
-        *p = tolower(*p);
-    }
+	pkg_arch_to_legacy(oi->abi, oi->altabi, sizeof(oi->abi));
 
 	if (oi->ostype == OS_DRAGONFLY) {
 		size_t dsz;
 
 		dsz = strlen(oi->abi);
-		if (strncasecmp(oi->abi, "DragonFly", 9) == 0) {
-			for (int i = 0; i < dsz; i++)
-				oi->abi[i] = tolower(oi->abi[i]);
-			return (0);
-		}
-	}
-
-	/* Translate architecture string back to regular OS one */
-	char *arch_tweak = strchr(oi->abi, ':');
-	if (arch_tweak == NULL)
+		for (int i = 0; i < dsz; i++)
+			oi->abi[i] = tolower(oi->abi[i]);
 		return (0);
-	arch_tweak++;
-	arch_tweak = strchr(arch_tweak, ':');
-	if (arch_tweak == NULL)
-		return (0);
-	arch_tweak++;
-	for (struct arch_trans *arch_trans = machine_arch_translation; arch_trans->elftype != NULL;
-	    arch_trans++) {
-		if (STREQ(arch_tweak, arch_trans->elftype)) {
-			strlcpy(arch_tweak, arch_trans->archid,
-			    sizeof(oi->abi) - (arch_tweak - oi->abi));
-			oi->arch = xstrdup(arch_tweak);
-			break;
-		}
 	}
 
 	return (0);

libpkg/pkg_abi_macho.c
@@ -90,52 +90,12 @@ cputype_to_freebsd_machine_arch(const cpu_type_subtype_t cpu)
 	}
 }
 
-static const char *
-cputype_to_elfname(const cpu_type_subtype_t cpu)
-{
-	switch (cpu.type) {
-	case CPU_TYPE_ARM:
-		if (cpu.type_is64) {
-			return "aarch64:64";
-		} else {
-			switch(cpu.subtype_arm) {
-			case CPU_SUBTYPE_ARM_V7:
-			case CPU_SUBTYPE_ARM_V7S:
-			case CPU_SUBTYPE_ARM_V7K:
-			case CPU_SUBTYPE_ARM_V7M:
-			case CPU_SUBTYPE_ARM_V7EM:
-				return "armv7:32:el:eabi:hardfp";
-			case CPU_SUBTYPE_ARM_V6:
-			case CPU_SUBTYPE_ARM_V6M:
-				return "armv6:32:el:eabi:hardfp";
-			default:
-				return "arm:32";
-			}
-		}
-	case CPU_TYPE_POWERPC:
-		if (cpu.type_is64) {
-			return "powerpc:64:eb";
-		} else {
-			return "powerpc:32:eb";
-		}
-	case CPU_TYPE_X86:
-		if (cpu.type_is64) {
-			return "x86:64";
-		} else {
-			return "x86:32";
-		}
-	default:
-		return "other";
-	}
-}
-
-
 int
 pkg_get_myarch_macho(int fd, struct os_info *oi)
 {
 	ssize_t x;
-	char *dest = oi->altabi;
-	size_t sz = sizeof(oi->altabi);
+	char *dest = oi->abi;
+	size_t sz = sizeof(oi->abi);
 
 	macho_file_t *mf = 0;
 	build_version_t *bv = 0;
@@ -219,7 +179,7 @@ pkg_get_myarch_macho(int fd, struct os_info *oi)
 	if (bv) {
 		macho_version_t darwin;
 		map_platform_to_darwin(&darwin, bv->platform, bv->minos);
-		snprintf(dest, sz, "Darwin:%d:%s", darwin.major, cputype_to_elfname(mh.cpu));
+		snprintf(dest, sz, "Darwin:%d:%s", darwin.major, cputype_to_freebsd_machine_arch(mh.cpu));
 		if (oi) {
 			oi->name = xstrdup("Darwin");
 			oi->osversion = darwin.major * 100000 + darwin.minor * 1000 + darwin.patch;

commit 8301963b973653329aa62c37037d41825bca446b

libpkg: fix regressions in ELF arch parsing
Since c2fe22b4, most non-amd64 architectures are parsed inadequately
from ELF files. This commit fixes the regression for architectures
supported by FreeBSD 14.x.

It also adds tests for these architectures to prevent possible
regressions in the future.

Note that not all regressions in architecture parsing from c2fe22b4 have
been fixed. The mips variants for example remain broken even with this
commit due to lack of test binaries.

Sponsored by:	The FreeBSD Foundation

libpkg/pkg_elf.c
@@ -606,6 +606,71 @@ aeabi_parse_arm_attributes(void *data, size_t length)
 #undef MOVE
 }
 
+static const char *
+elf_parse_arch(os_type_t ostype, Elf *elf, GElf_Ehdr *ehdr)
+{
+	switch (ehdr->e_machine) {
+	case EM_386:
+		return ("i386");
+	case EM_X86_64:
+		switch (ostype) {
+		case OS_FREEBSD:
+			return ("amd64");
+		case OS_DRAGONFLY:
+			return ("x86:64");
+		default:
+			return ("x86_64");
+		}
+	case EM_AARCH64:
+		return ("aarch64");
+	case EM_ARM:
+		/* Only support EABI */
+		if ((ehdr->e_flags & EF_ARM_EABIMASK) == 0) {
+			return (NULL);
+		}
+
+		size_t shstrndx;
+		elf_getshdrstrndx(elf, &shstrndx);
+
+		GElf_Shdr shdr;
+		Elf_Scn *scn = NULL;
+		while ((scn = elf_nextscn(elf, scn)) != NULL) {
+			if (gelf_getshdr(scn, &shdr) != &shdr) {
+				break;
+			}
+			const char *sh_name = elf_strptr(elf, shstrndx, shdr.sh_name);
+			if (sh_name == NULL) {
+				continue;
+			}
+			if (STREQ(".ARM.attributes", sh_name)) {
+				Elf_Data *data = elf_getdata(scn, NULL);
+				return (aeabi_parse_arm_attributes(data->d_buf, data->d_size));
+			}
+		}
+		break;
+	case EM_PPC:
+		return ("powerpc");
+	case EM_PPC64:
+		switch (ehdr->e_ident[EI_DATA]) {
+		case ELFDATA2MSB:
+			return ("powerpc64");
+		case ELFDATA2LSB:
+			return ("powerpc64le");
+		}
+		break;
+	case EM_RISCV:
+		switch (ehdr->e_ident[EI_CLASS]) {
+		case ELFCLASS32:
+			return ("riscv32");
+		case ELFCLASS64:
+			return ("riscv64");
+		}
+		break;
+	}
+
+	return (NULL);
+}
+
 static bool
 elf_note_analyse(Elf_Data *data, GElf_Ehdr *elfhdr, struct os_info *oi)
 {
@@ -731,10 +796,8 @@ pkg_get_myarch_elfparse(int fd, struct os_info *oi)
 	Elf_Data *data;
 	Elf_Scn *scn = NULL;
 	int ret = EPKG_OK;
-	const char *arch,*abi, *endian_corres_str, *wordsize_corres_str, *fpu;
 	char *dest = oi->abi;
 	size_t sz = sizeof(oi->abi);
-	size_t dsz;
 
 	if (elf_version(EV_CURRENT) == EV_NONE) {
 		pkg_emit_error("ELF library initialization failed: %s",
@@ -777,20 +840,15 @@ pkg_get_myarch_elfparse(int fd, struct os_info *oi)
 		goto cleanup;
 	}
 
-	snprintf(dest, sz, "%s:%s", oi->name, oi->version);
+	const char *arch = elf_parse_arch(oi->ostype, elf, &elfhdr);
+	if (arch == NULL) {
+		ret = EPKG_FATAL;
+		pkg_emit_error("failed to determine the architecture");
+		goto cleanup;
+	}
+	oi->arch = xstrdup(arch);
 
-	wordsize_corres_str = elf_corres_to_string(wordsize_corres,
-	    (int)elfhdr.e_ident[EI_CLASS]);
-
-	if (oi->ostype == OS_FREEBSD && elfhdr.e_machine == EM_X86_64)
-		oi->arch = xstrdup("amd64");
-	else if (oi->ostype == OS_DRAGONFLY && elfhdr.e_machine == EM_X86_64)
-		oi->arch = xstrdup("x86:64");
-	else
-		oi->arch = xstrdup(elf_corres_to_string(mach_corres, (int) elfhdr.e_machine));
-
-	dsz = strlen(dest);
-	snprintf(dest + dsz, sz - dsz, ":%s", oi->arch);
+	snprintf(dest, sz, "%s:%s:%s", oi->name, oi->version, oi->arch);
 
 cleanup:
 	if (elf != NULL)

libpkg/private/elf_tables.h
@@ -50,26 +50,6 @@ static const struct _elf_corres wordsize_corres[] = {
 	{ -1, NULL},
 };
 
-static const struct _elf_corres endian_corres[] = {
-	{ ELFDATA2MSB, "eb" },
-	{ ELFDATA2LSB, "el" },
-	{ -1, NULL}
-};
-
-static const struct _elf_corres os_corres[] = {
-	{ ELFOSABI_FREEBSD, "freebsd" },
-	{ -1, NULL }
-};
-
-#ifndef EF_MIPS_ABI
-#define EF_MIPS_ABI	0x0000F000
-#endif
-#ifndef EF_ARM_VFP_FLOAT
-#define EF_ARM_VFP_FLOAT	0x00000400
-#endif
-#define E_MIPS_ABI_O32	0x00001000
-#define E_MIPS_ABI_N32	0x00000020
-
 #define NT_VERSION	1
 #define NT_ARCH	2
 #define NT_GNU_ABI_TAG	1

tests/Makefile.autosetup
@@ -68,13 +68,24 @@ TESTS_SH= \
 	frontend/triggers.sh
 
 #
-# Those files simple binaries obtained from
+# These files are mostly simple binaries obtained from
 # int main(void) { return 0; }
 #
+# The freebsd-*.bin files are copies of /usr/bin/uname from official
+# 14.1 release artifacts for the given architecture.
+
 
 TESTS_SHELL_BINS= \
 	frontend/dfly.bin \
-	frontend/fbsd.bin \
+	frontend/freebsd-aarch64.bin \
+	frontend/freebsd-amd64.bin \
+	frontend/freebsd-armv6.bin \
+	frontend/freebsd-armv7.bin \
+	frontend/freebsd-i386.bin \
+	frontend/freebsd-powerpc.bin \
+	frontend/freebsd-powerpc64.bin \
+	frontend/freebsd-powerpc64le.bin \
+	frontend/freebsd-riscv64.bin \
 	frontend/linux.bin \
 	frontend/macos.bin \
 	frontend/macos106.bin \

tests/frontend/abi.sh
@@ -59,15 +59,96 @@ override_body() {
 
 elfparse_body() {
 	# ELF parsing now works across platforms
-	_expected="FreeBSD:13:amd64\n"
-	atf_check \
-		-o inline:"${_expected}" \
-		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/fbsd.bin config abi
 
-	_expected="freebsd:13:x86:64\n"
+	_expected="FreeBSD:14:aarch64\n"
 	atf_check \
 		-o inline:"${_expected}" \
-		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/fbsd.bin config altabi
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-aarch64.bin config abi
+
+	_expected="freebsd:14:aarch64:64\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-aarch64.bin config altabi
+
+	_expected="FreeBSD:14:amd64\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-amd64.bin config abi
+
+	_expected="freebsd:14:x86:64\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-amd64.bin config altabi
+
+	_expected="FreeBSD:13:armv6\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-armv6.bin config abi
+
+	_expected="freebsd:13:armv6:32:el:eabi:hardfp\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-armv6.bin config altabi
+
+	_expected="FreeBSD:14:armv7\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-armv7.bin config abi
+
+	_expected="freebsd:14:armv7:32:el:eabi:hardfp\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-armv7.bin config altabi
+
+	_expected="FreeBSD:14:i386\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-i386.bin config abi
+
+	_expected="freebsd:14:x86:32\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-i386.bin config altabi
+
+	_expected="FreeBSD:14:powerpc\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc.bin config abi
+
+	_expected="freebsd:14:powerpc:32:eb\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc.bin config altabi
+
+	_expected="FreeBSD:14:powerpc64\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc64.bin config abi
+
+	_expected="freebsd:14:powerpc:64:eb\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc64.bin config altabi
+
+	_expected="FreeBSD:14:powerpc64le\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc64le.bin config abi
+
+	_expected="freebsd:14:powerpc:64:el\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-powerpc64le.bin config altabi
+
+	_expected="FreeBSD:14:riscv64\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-riscv64.bin config abi
+
+	_expected="freebsd:14:riscv:64:hf\n"
+	atf_check \
+		-o inline:"${_expected}" \
+		pkg -o IGNORE_OSMAJOR=1 -o ABI_FILE=$(atf_get_srcdir)/freebsd-riscv64.bin config altabi
 
 	_expected="dragonfly:5.10:x86:64\n"
 	atf_check \