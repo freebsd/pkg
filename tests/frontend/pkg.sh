b2566adec1b1f0283244a733c2160c1a4adebdda
Baptiste Daroussin <bapt@FreeBSD.org>
2024-11-25 09:46:00 +0100

    tests: fix altabi matching on linux

--- tests/frontend/pkg.sh
+++ tests/frontend/pkg.sh
@@ -21,6 +21,15 @@ pkg_no_database_body() {
 
 pkg_config_defaults_body()
 {
+	case "${OS}" in
+	FreeBSD|DragonFly)
+		MATCH_ALTABI='^ *ALTABI = "[a-zA-Z0-9]+:[a-z\.A-Z0-9]+:[a-zA-Z0-9]+:[a-zA-Z0-9:]+";$'
+		;;
+	*)
+		MATCH_ALTABI='^ *ALTABI = "[a-zA-Z0-9]+:[a-z\.A-Z0-9]+:[a-zA-Z0-9_]+;$'
+		;;
+	esac
+
 	atf_check \
 	    -o match:'^ *PKG_DBDIR = "/var/db/pkg";$' \
 	    -o match:'^ *PKG_CACHEDIR = "/var/cache/pkg";$' \
@@ -30,7 +39,7 @@ pkg_config_defaults_body()
 	    -o match:'^ *ASSUME_ALWAYS_YES = false;$' \
 	    -o match:'^ *PLIST_KEYWORDS_DIR = "";$' \
 	    -o match:'^ *SYSLOG = true;$' \
-	    -o match:'^ *ALTABI = "[a-zA-Z0-9]+:[a-z\.A-Z0-9]+:[a-zA-Z0-9]+:[a-zA-Z0-9:]+";$' \
+	    -o match:"${MATCH_ABI}" \
 	    -o match:'^ *DEVELOPER_MODE = false;$' \
 	    -o match:'^ *VULNXML_SITE = "https://vuxml.freebsd.org/freebsd/vuln.xml.xz";$' \
 	    -o match:'^ *FETCH_RETRY = 3;$' \
