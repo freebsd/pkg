0044ce03838ee7fdedcb26c703c15f635e366f19
Baptiste Daroussin <bapt@FreeBSD.org>
2025-01-21 15:19:14 +0100

    add missing header

---
+++ libpkg/private/add.h
@@ -0,0 +1,19 @@
+/*-
+ * Copyright (c) 2024-2025 Baptiste Daroussin <bapt@FreeBSD.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <private/pkg.h>
+
+struct pkg_add_context {
+	int rootfd;
+	c_charv_t *symlinks_allowed;
+	struct pkgdb *db;
+	struct pkg *pkg;
+};
+
+struct tempdir *open_tempdir(struct pkg_add_context *, const char *path);
+
