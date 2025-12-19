freebsd/pkg

commit 08bf9b3d79b127b127ecf597286ba14c016af8c4

pkgdb: track mtime in pkgdb
Store the file mtime in package manifests in order to be agnostic
of the container format (tar).

libpkg/pkg_create.c
@@ -132,6 +132,9 @@ pkg_create_from_dir(struct pkg *pkg, const char *root,
 				}
 				file->symlink_target[linklen] = '\0';
 			}
+
+			file->time[0] = st.st_atim;
+			file->time[1] = st.st_mtim;
 		}
 
 		counter_count();

docs/pkg-query.8
@@ -223,7 +223,7 @@ for the package origin, and
 for the package version.
 .It Cm \&%C
 Expands to the list of categories the matched package belongs to.
-.It Cm \&%F Ns Op psugmft
+.It Cm \&%F Ns Op psugmftl
 Expands to the list of files of the matched package, where
 .Cm p
 stands for path,
@@ -236,7 +236,9 @@ for group,
 .Cm m
 for mode (permissions),
 .Cm f
-for file flags, and
+for file flags,
+.Cm l
+for last modification time, and
 .Cm t
 for the symlink target (or empty string if no symlink).
 .It Cm \&%D

docs/pkg_printf.3
@@ -512,6 +512,9 @@ File flags [string]
 .It Cm %Fg
 File ownership: group name [string]
 .Vt struct pkg_file *
+.It Cm %Fl
+File last modification time [epoch]
+.Vt struct pkg_file *
 .It Cm %\^Fn
 File path name [string]
 .Vt struct pkg_file *

libpkg/pkg.c
@@ -493,12 +493,13 @@ pkg_addrdep(struct pkg *pkg, const char *name, const char *origin, const char *v
 int
 pkg_addfile(struct pkg *pkg, const char *path, const char *sum, bool check_duplicates)
 {
-	return (pkg_addfile_attr(pkg, path, sum, NULL, NULL, 0, 0, NULL, check_duplicates));
+	return (pkg_addfile_attr(pkg, path, sum, NULL, NULL, 0, 0, 0, NULL, check_duplicates));
 }
 
 int
 pkg_addfile_attr(struct pkg *pkg, const char *path, const char *sum,
-		 const char *uname, const char *gname, mode_t perm, u_long fflags,
+		 const char *uname, const char *gname, mode_t perm,
+		 u_long fflags, time_t mtime,
 		 const char *symlink_target, bool check_duplicates)
 {
 	struct pkg_file *f = NULL;
@@ -540,6 +541,9 @@ pkg_addfile_attr(struct pkg *pkg, const char *path, const char *sum,
 
 	if (symlink_target != NULL)
 		strlcpy(f->symlink_target, symlink_target, sizeof(f->symlink_target));
+
+	if (mtime > 0)
+		f->time[1].tv_sec = mtime;
 
 	pkghash_safe_add(pkg->filehash, f->path, f, NULL);
 	DL_APPEND(pkg->files, f);

libpkg/pkg_add.c
@@ -374,15 +374,15 @@ fill_timespec_buf(const struct stat *aest, struct timespec tspec[2])
 {
 #ifdef HAVE_STRUCT_STAT_ST_MTIM
 	tspec[0].tv_sec = aest->st_atim.tv_sec;
-	tspec[0].tv_nsec = aest->st_atim.tv_nsec;
+	tspec[0].tv_nsec = 0;
 	tspec[1].tv_sec = aest->st_mtim.tv_sec;
-	tspec[1].tv_nsec = aest->st_mtim.tv_nsec;
+	tspec[1].tv_nsec = 0;
 #else
 # if defined(_DARWIN_C_SOURCE) || defined(__APPLE__)
 	tspec[0].tv_sec = aest->st_atimespec.tv_sec;
-	tspec[0].tv_nsec = aest->st_atimespec.tv_nsec;
+	tspec[0].tv_nsec = 0;
 	tspec[1].tv_sec = aest->st_mtimespec.tv_sec;
-	tspec[1].tv_nsec = aest->st_mtimespec.tv_nsec;
+	tspec[1].tv_nsec = 0;
 # else
 	/* Portable unix version */
 	tspec[0].tv_sec = aest->st_atime;

libpkg/pkg_manifest.c
@@ -580,6 +580,7 @@ pkg_set_files_from_object(struct pkg *pkg, const ucl_object_t *obj)
 	u_long fflags = 0;
 	char *fname = NULL;
 	const char *key, *okey;
+	time_t mtime = 0;
 
 	okey = ucl_object_key(obj);
 	if (okey == NULL)
@@ -618,6 +619,15 @@ pkg_set_files_from_object(struct pkg *pkg, const ucl_object_t *obj)
 #endif
 		} else if (STRIEQ(key, "symlink_target") && cur->type == UCL_STRING) {
 			symlink_target = ucl_object_tostring(cur);
+		} else if (STRIEQ(key, "mtime") &&
+			   (cur->type == UCL_STRING || cur->type == UCL_INT)) {
+			errno = 0;
+			mtime = strtoll(ucl_object_tostring_forced(cur), NULL, 10);
+			if (mtime == 0 && errno != 0) {
+				pkg_emit_errno("strtoll: invalid epoch value",
+					       ucl_object_tostring_forced(cur));
+				continue;
+			}
 		} else {
 			dbg(1, "Skipping unknown key for file(%s): %s",
 			    fname, key);
@@ -625,7 +635,7 @@ pkg_set_files_from_object(struct pkg *pkg, const ucl_object_t *obj)
 	}
 
 	pkg_addfile_attr(pkg, fname, sum, uname, gname, perm, fflags,
-			 symlink_target, false);
+			 mtime, symlink_target, false);
 	free(fname);
 
 	return (EPKG_OK);
@@ -1176,6 +1186,11 @@ pkg_emit_object(struct pkg *pkg, short flags)
 					ucl_object_insert_key(file_attrs,
 							      ucl_object_fromstring(file->symlink_target),
 							      "symlink_target", 0, false);
+				}
+				if (file->time[1].tv_sec > 0) {
+					ucl_object_insert_key(file_attrs,
+							      ucl_object_fromint(file->time[1].tv_sec),
+							      "mtime", 0, false);
 				}
 
 				urlencode(dp, &tmpsbuf);

libpkg/pkg_ports.c
@@ -407,12 +407,12 @@ meta_file(struct plist *p, char *line, struct file_attr *a, bool is_config)
 				       a->owner ? a->owner : p->uname,
 				       a->group ? a->group : p->gname,
 				       a->mode ? a->mode : p->perm,
-				       a->fflags,
+				       a->fflags, st.st_mtim.tv_sec,
 				       linklen > 0 ? symlink_target : NULL,
 				       true);
 	} else {
 		ret = pkg_addfile_attr(p->pkg, path, buf, p->uname,
-				       p->gname, p->perm, 0,
+				       p->gname, p->perm, 0, st.st_mtim.tv_sec,
 				       linklen > 0 ? symlink_target : NULL,
 				       true);
 	}

libpkg/pkg_printf.c
@@ -76,6 +76,7 @@
  * Ff pkg_file     File flags of file
  * Fg pkg_file     Group owner of file
  * Fk pkg_file     Keep flag
+ * Fl pkg_file     File modification time
  * Fn pkg_file     File path name
  * Fp pkg_file     File permissions
  * Fs pkg_file     File SHA256 checksum
@@ -319,6 +320,15 @@ static const struct pkg_printf_fmt	fmt[] = {
 		false,
 		PP_PKG|PP_F,
 		&format_file_group,
+	},
+	[PP_PKG_FILE_MTIME] =
+	{
+		'F',
+		'l',
+		false,
+		false,
+		PP_PKG|PP_F,
+		&format_file_mtime,
 	},
 	[PP_PKG_FILE_PATH] =
 	{
@@ -885,6 +895,24 @@ format_fflags(xstring *buf, u_long fflags, struct percent_esc *p)
 	return (ret);
 }
 
+static xstring *
+format_time_t(xstring *buf, time_t timestamp, struct percent_esc *p)
+{
+	fflush(p->item_fmt->fp);
+	if (strlen(p->item_fmt->buf) == 0)
+		return (int_val(buf, timestamp, p));
+	else {
+		char	 buffer[1024];
+		time_t	 tsv;
+
+		tsv = timestamp;
+		strftime(buffer, sizeof(buffer), p->item_fmt->buf,
+			 localtime(&tsv));
+		fprintf(buf->fp, "%s", buffer);
+	}
+	return (buf);
+}
+
 /*
  * Note: List values -- special behaviour with ? and # modifiers.
  * Affects %A %B %C %D %F %G %L %O %U %b %d %r
@@ -1165,6 +1193,17 @@ format_files(xstring *buf, const void *data, struct percent_esc *p)
 		}
 	}
 	return (buf);
+}
+
+/*
+ * %Fm -- File modification time.
+ */
+xstring *
+format_file_mtime(xstring *buf, const void *data, struct percent_esc *p)
+{
+	const struct pkg_file *file = data;
+
+	return (format_time_t(buf, file->time[1].tv_sec, p));
 }
 
 /*
@@ -1930,19 +1969,7 @@ format_install_tstamp(xstring *buf, const void *data, struct percent_esc *p)
 {
 	const struct pkg	*pkg = data;
 
-	fflush(p->item_fmt->fp);
-	if (strlen(p->item_fmt->buf) == 0)
-		return (int_val(buf, pkg->timestamp, p));
-	else {
-		char	 buffer[1024];
-		time_t	 tsv;
-
-		tsv = (time_t)pkg->timestamp;
-		strftime(buffer, sizeof(buffer), p->item_fmt->buf,
-			 localtime(&tsv));
-		fprintf(buf->fp, "%s", buffer);
-	}
-	return (buf);
+	return (format_time_t(buf, pkg->timestamp, p));
 }
 
 /*

commit b8a3ff0210e32bac2377d12fb6de410d009dc291

pkg_create: use -t specified timestamp for MANIFEST file timestamps
This is necessary to provide pkg archive reproducibility after
https://github.com/freebsd/pkg/commit/08bf9b3d79b127b127ecf597286ba14c016af8c4.

Fixes:         08bf9b3
Signed-off-by: Siva Mahadevan <me@svmhdvn.name>
Sponsored by:  The FreeBSD Foundation

libpkg/pkg_create.c
@@ -137,8 +137,13 @@ pkg_create_from_dir(struct pkg *pkg, const char *root,
 				file->symlink_target[linklen] = '\0';
 			}
 
-			file->time[0] = st.st_atim;
-			file->time[1] = st.st_mtim;
+			if (pc->timestamp > (time_t)-1) {
+				file->time[0].tv_sec = pc->timestamp;
+				file->time[1].tv_sec = pc->timestamp;
+			} else {
+				file->time[0] = st.st_atim;
+				file->time[1] = st.st_mtim;
+			}
 		}
 
 		counter_count();