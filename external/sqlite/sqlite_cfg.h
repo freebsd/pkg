2e556efbf219c07d4e6e57da93df2cc8d0fcb499
Baptiste Daroussin <bapt@FreeBSD.org>
2026-01-26 15:36:31 +0100

    compat: stop dynamically checking more standard functions

--- external/sqlite/sqlite_cfg.h
+++ external/sqlite/sqlite_cfg.h
@@ -1,5 +1,14 @@
 #ifndef _SQLITE_CFG_H
 #define _SQLITE_CFG_H
+#define HAVE_FCHOWN 1
+#define HAVE_GMTIME_R 1
 #define HAVE_ISNAN 1
+#define HAVE_LOCALTIME_R 1
+#define HAVE_LSTAT 1
+#define HAVE_READLINK 1
+#define HAVE_STRERROR_R 1
+#ifndef __APPLE__
+#define HAVE_FDATASYNC 1
+#endif
 #include "sqlite_generated.h"
 #endif
