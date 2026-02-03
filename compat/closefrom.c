9cd3f1b9b6191c05a06828c2efdee16bb493fc12
Baptiste Daroussin <bapt@FreeBSD.org>
2026-02-03 11:22:23 +0100

    dirent.h: stop testing it, it is available everywhere

--- compat/closefrom.c
+++ compat/closefrom.c
@@ -30,22 +30,8 @@
 #include <stddef.h>
 #include <string.h>
 #include <unistd.h>
-#ifdef HAVE_DIRENT_H
-# include <dirent.h>
-# define NAMLEN(dirent) strlen((dirent)->d_name)
-#else
-# define dirent direct
-# define NAMLEN(dirent) (dirent)->d_namlen
-# ifdef HAVE_SYS_NDIR_H
-#  include <sys/ndir.h>
-# endif
-# ifdef HAVE_SYS_DIR_H
-#  include <sys/dir.h>
-# endif
-# ifdef HAVE_NDIR_H
-#  include <ndir.h>
-# endif
-#endif
+#include <dirent.h>
+#define NAMLEN(dirent) strlen((dirent)->d_name)
 
 #ifndef OPEN_MAX
 # define OPEN_MAX	256
