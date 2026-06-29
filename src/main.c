3fa3c391615534b5182a0c829410e00b94c08c10
Dag-Erling Smørgrav <des@FreeBSD.org>
2026-06-29 03:56:15 +0200

    Make PKG_CONFIG_FILE a config variable
    This makes it possible to run `pkg config PKG_CONFIG_FILE` and get back
    the correct path in all instances.  Attempts to change it in the
    configuration will be ignored.

--- src/main.c
+++ src/main.c
@@ -683,9 +683,6 @@ main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 	}
 
-	if (conffile == NULL)
-		conffile = getenv("PKG_CONFIG_FILE");
-
 	if (pkg_ini(conffile, reposdir, init_flags) != EPKG_OK)
 		errx(EXIT_FAILURE, "Cannot parse configuration file!");
 
